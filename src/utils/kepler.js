864fa8bd7df4247230154bcdaa5cb55a829e7bb2
puneet2019 <puneet@persistence.one>
2021-03-25 12:46:03 +0530

    adding testnet config to keplr.

--- src/utils/kepler.js
+++ src/utils/kepler.js
@@ -18,19 +18,19 @@ async function KeplerWallet() {
                 // If the same chain id is already registered, it will resolve and not require the user interactions.
                 await window.keplr.experimentalSuggestChain({
                     // Chain-id of the Cosmos SDK chain.
-                    chainId: "cosmoshub-3",
+                    chainId: "test-core-1",
                     // The name of the chain to be displayed to the user.
-                    chainName: "Cosmos",
+                    chainName: "Persistence Testnet",
                     // RPC endpoint of the chain.
-                    rpc: "https://node-cosmoshub-3.keplr.app/rpc",
+                    rpc: "http://128.199.29.15:26657",
                     // REST endpoint of the chain.
-                    rest: "https://node-cosmoshub-3.keplr.app/rest",
+                    rest: "http://128.199.29.15:1317",
                     // Staking coin information
                     stakeCurrency: {
                         // Coin denomination to be displayed to the user.
-                        coinDenom: "ATOM",
+                        coinDenom: "XPRT",
                         // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-                        coinMinimalDenom: "uatom",
+                        coinMinimalDenom: "uxprt",
                         // # of decimal points to convert minimal denomination to user-facing denomination.
                         coinDecimals: 6,
                         // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
@@ -44,7 +44,7 @@ async function KeplerWallet() {
                     bip44: {
                         // You can only set the coin type of BIP44.
                         // 'Purpose' is fixed to 44.
-                        coinType: 118,
+                        coinType: 750,
                     },
                     // Bech32 configuration to show the address to user.
                     // This field is the interface of
@@ -57,19 +57,19 @@ async function KeplerWallet() {
                     //   bech32PrefixConsPub: string;
                     // }
                     bech32Config: {
-                        bech32PrefixAccAddr: "cosmos",
-                        bech32PrefixAccPub: "cosmospub",
-                        bech32PrefixValAddr: "cosmosvaloper",
-                        bech32PrefixValPub: "cosmosvaloperpub",
-                        bech32PrefixConsAddr: "cosmosvalcons",
-                        bech32PrefixConsPub: "cosmosvalconspub"
+                        bech32PrefixAccAddr: "persistence",
+                        bech32PrefixAccPub: "persistencepub",
+                        bech32PrefixValAddr: "persistencevaloper",
+                        bech32PrefixValPub: "persistencevaloperpub",
+                        bech32PrefixConsAddr: "persistencevalcons",
+                        bech32PrefixConsPub: "persistencevalconspub"
                     },
                     // List of all coin/tokens used in this chain.
                     currencies: [{
                         // Coin denomination to be displayed to the user.
-                        coinDenom: "ATOM",
+                        coinDenom: "XPRT",
                         // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-                        coinMinimalDenom: "uatom",
+                        coinMinimalDenom: "uxprt",
                         // # of decimal points to convert minimal denomination to user-facing denomination.
                         coinDecimals: 6,
                         // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
@@ -79,9 +79,9 @@ async function KeplerWallet() {
                     // List of coin/tokens used as a fee token in this chain.
                     feeCurrencies: [{
                         // Coin denomination to be displayed to the user.
-                        coinDenom: "ATOM",
+                        coinDenom: "XPRT",
                         // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-                        coinMinimalDenom: "uatom",
+                        coinMinimalDenom: "uxprt",
                         // # of decimal points to convert minimal denomination to user-facing denomination.
                         coinDecimals: 6,
                         // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
@@ -93,7 +93,7 @@ async function KeplerWallet() {
                     // Ideally, it is recommended to be the same with BIP44 path's coin type.
                     // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
                     // So, this is separated to support such chains.
-                    coinType: 118,
+                    coinType: 750,
                     // (Optional) This is used to set the fee of the transaction.
                     // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
                     // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
@@ -112,7 +112,7 @@ async function KeplerWallet() {
         }
     }
 
-    const chainId = "cosmoshub-3";
+    const chainId = "test-core-1";
 
     // You should request Keplr to enable the wallet.
     // This method will ask the user whether or not to allow access if they haven't visited this website.
@@ -130,7 +130,7 @@ async function KeplerWallet() {
 
     // Initialize the gaia api with the offline signer that is injected by Keplr extension.
     const cosmJS = new SigningCosmosClient(
-        "https://node-cosmoshub-3.keplr.app/rest",
+        "http://128.199.29.15:1317",
         accounts[0].address,
         offlineSigner,
     );
