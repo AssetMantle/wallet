3d5cd2770b3fc953666b17e235456521a3846462
puneet2019 <puneet@persistence.one>
2021-04-01 16:23:20 +0530

    adding setWithdrawAddressMsg to protoMsgHelper.js.

--- src/utils/protoMsgHelper.js
+++ src/utils/protoMsgHelper.js
@@ -1,12 +1,16 @@
 import {MsgSend} from "@cosmjs/stargate/build/codec/cosmos/bank/v1beta1/tx";
 import {MsgBeginRedelegate, MsgDelegate, MsgUndelegate} from "@cosmjs/stargate/build/codec/cosmos/staking/v1beta1/tx";
-import {MsgWithdrawDelegatorReward} from "@cosmjs/stargate/build/codec/cosmos/distribution/v1beta1/tx";
+import {
+    MsgSetWithdrawAddress,
+    MsgWithdrawDelegatorReward
+} from "@cosmjs/stargate/build/codec/cosmos/distribution/v1beta1/tx";
 
 const msgSendTypeUrl = "/cosmos.bank.v1beta1.MsgSend"
 const msgDelegateTypeUrl = "/cosmos.staking.v1beta1.MsgDelegate"
 const msgRedelegateTypeUrl = "/cosmos.staking.v1beta1.MsgBeginRedelegate"
 const msgUnbondTypeUrl = "/cosmos.staking.v1beta1.MsgUndelegate"
 const msgWithdrawRewardsTypeUrl = "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward"
+const msgSetWithdrawAddressTypeUrl = "/cosmos.distribution.v1beta1.MsgSetWithdrawAddress"
 
 
 function SendMsg(fromAddress, toAddress, amount) {
@@ -80,4 +84,15 @@ function WithdrawMsg(delegatorAddress, validatorAddress) {
         }),
     };
 }
-export {SendMsg, DelegateMsg, RedelegateMsg, UnbondMsg, WithdrawMsg}
+
+function SetWithDrawAddressMsg(delegatorAddress, withdrawAddress) {
+    return {
+        typeUrl: msgSetWithdrawAddressTypeUrl,
+        value: MsgSetWithdrawAddress.fromPartial({
+            delegatorAddress: delegatorAddress,
+            withdrawAddress: withdrawAddress,
+        }),
+    };
+}
+
+export {SendMsg, DelegateMsg, RedelegateMsg, UnbondMsg, WithdrawMsg, SetWithDrawAddressMsg}
