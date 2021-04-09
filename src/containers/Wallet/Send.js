af7afbac986e9571008c0664f7e8e6bb1216593c
blackpanther8132 <blackpanther8132@gmail.com>
2021-04-09 15:21:44 +0530

    changed memo field look

--- src/containers/Wallet/Send.js
+++ src/containers/Wallet/Send.js
@@ -268,9 +268,12 @@ const Send = (props) => {
                     {mode === "normal" ?
                         <div className="form-field">
                             <p className="label">{t("MEMO")}</p>
-                            <Form.Control as="textarea" rows={3} name="memo"
-                                          placeholder={t("ENTER_MEMO")}
-                                          required={false}/>
+                            <Form.Control
+                                type="text"
+                                name="memo"
+                                placeholder={t("ENTER_MEMO")}
+                                required={false}
+                            />
                         </div> : null
                     }
                     {keplerError !== '' ?
