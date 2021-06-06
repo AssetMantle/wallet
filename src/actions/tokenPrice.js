3edc0f414cc771a6867e8de2423061d83088656d
ashish-jiraiya <ashishkvinshamla@gmail.com>
2021-06-06 17:22:54 +0530

    add coin gecko api for price.

--- src/actions/tokenPrice.js
+++ src/actions/tokenPrice.js
@@ -1,11 +1,10 @@
-import Axios from 'axios';
-
 import {
     TOKEN_PRICE_FETCH_ERROR,
     TOKEN_PRICE_FETCH_SUCCESS,
 } from "../constants/tokenPrice";
+import CoinGecko from 'coingecko-api';
 
-const PROXY_API = process.env.REACT_APP_PROXY_API;
+const CoinGeckoClient = new CoinGecko();
 
 export const fetchTokenPriceSuccess = (data) => {
     return {
@@ -21,16 +20,14 @@ export const fetchTokenPriceError = (data) => {
 };
 
 export const fetchTokenPrice = () => (dispatch) => {
-    const url = `${PROXY_API}/ascendExTicker?symbol=XPRT/USDT`;
-    Axios.get(url)
-        .then((res) => {
-            if (res.data.data.close) {
-                dispatch(fetchTokenPriceSuccess(res.data.data.close));
-            }
-        })
-        .catch((error) => {
-            dispatch(fetchTokenPriceError(error.response
-                ? error.response.data.message
-                : error.message));
-        });
+
+    CoinGeckoClient.simple.price({ids:"persistence"}).then((res) => {
+        if (res.data.persistence.usd) {
+            dispatch(fetchTokenPriceSuccess(res.data.persistence.usd));
+        }
+    }).catch((error) => {
+        dispatch(fetchTokenPriceError(error.response
+            ? error.response.data.message
+            : error.message));
+    });
 };
