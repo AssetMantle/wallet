407e3716c7fc8a974939a78047916ab85d66642e
Ankit Kumar <ankit.kr554@gmail.com>
2022-05-26 13:17:11 +0530

    keplr redelegate

--- src/store/actions/transactions/keplr.js
+++ src/store/actions/transactions/keplr.js
@@ -2,10 +2,15 @@ import transactions from "../../../utils/transactions";
 import {fee} from "../../../utils/aminoMsgHelper";
 import {showTxResultModal, txFailed, txInProgress, txResponse, txSuccess} from "./common";
 import * as Sentry from "@sentry/browser";
+import {GasInfo} from "../../../config";
 
 export const keplrSubmit = (messages = "") => (dispatch, getState) => {
     dispatch(txInProgress());
-    const response = transactions.TransactionWithKeplr(messages, fee(0, 250000),"");
+    let gas = GasInfo.gas;
+    if (getState().common.txName.value.name !== "reDelegate") {
+        gas = GasInfo.redelegateGas;
+    }
+    const response = transactions.TransactionWithKeplr(messages, fee(0, gas),"");
     response.then(result => {
         if (getState().common.txName.value.name !== "send" && getState().common.txName.value.name !== "ibc") {
             dispatch(getState().common.txInfo.value.modal);
