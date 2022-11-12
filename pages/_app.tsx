26add36a122fb8b2e1ea7470d014497a7c20def1
Kamlesh P <kamlesh@assetmantle.one>
2022-11-12 15:19:54 +0530

    cosmos-app with eslint & cosmjs packages for tsgen

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WalletProvider } from '@cosmos-kit/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { defaultTheme, chainName } from '../config';
+import { wallets } from '@cosmos-kit/keplr';
+
+import { SignerOptions } from '@cosmos-kit/core';
+import { chains, assets } from 'chain-registry';
+
+function CreateCosmosApp({ Component, pageProps }: AppProps) {
+  const signerOptions: SignerOptions = {
+    // stargate: (_chain: Chain) => {
+    //   return getSigningCosmosClientOptions();
+    // }
+  };
+
+  return (
+    <ChakraProvider theme={defaultTheme}>
+      <WalletProvider
+        chains={chains}
+        assetLists={assets}
+        wallets={wallets}
+        signerOptions={signerOptions}
+      >
+        <Component {...pageProps} />
+      </WalletProvider>
+    </ChakraProvider>
+  );
+}
+
+export default CreateCosmosApp;
