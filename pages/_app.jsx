39f38f8b156b321f0fe8ca3a47aa0edee41d65a1
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-14 20:16:59 +0600

    🛠️📕💅configured

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { wallets as cosmostationWallets } from "@cosmos-kit/cosmostation";
 import { wallets as leapwallets } from "@cosmos-kit/leap";
 import { wallets as keplrWallets2 } from "@cosmos-kit/keplr";
 import { WalletProvider } from "@cosmos-kit/react";
 import { defaultTheme, wallets as keplrWallets } from "../config";
-import "../styles/globals.css";
+
+import "../config/styles/index.scss";
 
 import { assets, chains } from "chain-registry";
+import Layout from "../components/Layout";
 
 function CreateCosmosApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   const signerOptions = {
     // stargate: (_chain: Chain) => {
     //   return getSigningCosmosClientOptions();
@@ -25,7 +31,9 @@ function CreateCosmosApp({ Component, pageProps }) {
         wallets={[...keplrWallets2, ...leapwallets, ...cosmostationWallets]}
         signerOptions={signerOptions}
       >
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </WalletProvider>
     </ChakraProvider>
   );
