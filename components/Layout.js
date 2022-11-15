8c5b330890a35e6cb9233784cd590fc668f9393b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-15 12:19:25 +0600

    🛠️updated

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../views/Header";
 
 export default function Layout({ children }) {
+  const [Connected, setConnected] = useState(false);
   return (
     <>
-      <Header />
+      <Header Connected={Connected} setConnected={setConnected} />
       <main className="container-lg">
         <div className="row">
           <div className="col-3"></div>
