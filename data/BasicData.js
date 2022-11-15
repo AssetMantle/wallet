cff5b7748648a67eb855d0f81cf946043fea907e
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-15 12:18:36 +0600

    📕updated

---
+++ data/BasicData.js
@@ -0,0 +1,29 @@
+const BasicData = {
+  logo: "/logo.svg",
+  title: "Mantle Wallet",
+  navs: [
+    {
+      title: "Wallet",
+      href: "/",
+      variant: "am-link",
+    },
+    {
+      title: "Delegate",
+      href: "/",
+      variant: "am-link",
+    },
+    {
+      title: "Explorer",
+      href: "https://explorer.assetmantle.one/",
+      variant: "am-link",
+      target: "_blank",
+    },
+    {
+      title: "IBC Transaction",
+      href: "/",
+      variant: "am-link",
+    },
+  ],
+};
+
+export default BasicData;
