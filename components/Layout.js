c5a27e3c2da8d9319c082613cd6c6de7d2043787
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-15 12:54:39 +0600

    🛠️added side bars

--- components/Layout.js
+++ components/Layout.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import Balance from "../views/Balance";
 import Header from "../views/Header";
+import Portfolio from "../views/Portfolio";
+import StakedToken from "../views/StakedToken";
 
 export default function Layout({ children }) {
   const [Connected, setConnected] = useState(false);
   return (
     <>
       <Header Connected={Connected} setConnected={setConnected} />
-      <main className="container-lg">
+      <main className="container-lg pt-5">
         <div className="row">
-          <div className="col-3"></div>
+          <div className="col-3 d-flex flex-column gap-4">
+            <Balance />
+            <Portfolio />
+          </div>
           <div className="col-6">{children}</div>
-          <div className="col-3"></div>
+          <div className="col-3">
+            <StakedToken />
+          </div>
         </div>
       </main>
     </>
