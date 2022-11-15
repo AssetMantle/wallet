8edfdd5d68d9ca72a1f26a4ac77e2e995fdaf706
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-15 12:54:02 +0600

    🛠️💅📕🏷️created & completed

---
+++ views/Balance.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BsInfoCircle } from "react-icons/bs";
+
+export default function Balance() {
+  const dataSet = {
+    totalBalance: "0000.23000",
+    price: "0.312812",
+  };
+  return (
+    <section className="rounded-5 p-4 bg-gray-800 width-100 d-flex flex-column gap-3">
+      <h4 className="body1 text-primary">Wallet balances</h4>
+      <div className="nav-bg p-3 rounded-4 gap-3">
+        <div className="d-flex flex-column gap-2">
+          <p className="caption d-flex gap-2 align-items-center">
+            Total{" "}
+            <span>
+              <BsInfoCircle />
+            </span>
+          </p>
+          <p className="caption">{dataSet.totalBalance} $MNTL</p>
+        </div>
+      </div>
+      <div className="nav-bg p-3 rounded-4 gap-3">
+        <div className="d-flex flex-column gap-2">
+          <p className="caption d-flex gap-2 align-items-center">
+            Current Price per $MNTL
+          </p>
+          <p className="caption">{dataSet.price} $MNTL</p>
+        </div>
+      </div>
+      <div className="nav-bg p-3 rounded-4 gap-3">
+        <div className="d-flex flex-column gap-2">
+          <p className="caption d-flex gap-2 align-items-center">
+            Current Value
+          </p>
+          <p className="caption">
+            {Number(dataSet.totalBalance) * Number(dataSet.price)} $MNTL
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
