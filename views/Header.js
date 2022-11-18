1099be780b5b76c2f92a2b7cd2f03939744c999b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-18 13:57:28 +0600

    🛠️updated

--- views/Header.js
+++ views/Header.js
@@ -1,35 +1,39 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { useState } from "react";
+import React from "react";
 import BasicData from "../data/BasicData";
 import { MdOutlineContentCopy } from "react-icons/md";
 import { RiKey2Fill } from "react-icons/ri";
 import { VscSignOut } from "react-icons/vsc";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header({ Connected, setConnected }) {
   const dataSet = {
-    profileImage:
-      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    profileImage: "/profile.avif",
     name: "User1234",
     balance: "0.34847",
     qrCode: "/qr-code.svg",
-    address: "ThequickbrownfoxjumpsoverthelazydogfIfthedogr",
+    address: "thequickbrownfoxjumpsoverthelazydogfIfthedogr",
   };
   return (
     <header className="nav-bg">
       <div className="container-lg d-flex align-items-center justify-content-between p-2">
-        <div className="d-flex" style={{ width: "min(196px,30%)" }}>
-          <img src={BasicData.logo} alt={BasicData.title} />
+        <div
+          className="d-flex position-relative"
+          style={{ width: "min(196px,30%)", aspectRatio: "196/34" }}
+        >
+          <Image layout="fill" src={BasicData.logo} alt={BasicData.title} />
         </div>
         <nav className="navbar-nav d-flex align-items-center flex-row gap-3">
           {React.Children.toArray(
             BasicData.navs.map((navItem) => (
-              <a
-                className={`${navItem.variant}`}
-                href={navItem.href}
-                target={navItem.target ? navItem.target : "_self"}
-              >
-                {navItem.title}
-              </a>
+              <Link href={navItem.href}>
+                <a
+                  className={`${navItem.variant}`}
+                  target={navItem.target ? navItem.target : "_self"}
+                >
+                  {navItem.title}
+                </a>
+              </Link>
             ))
           )}
           {Connected ? (
@@ -44,22 +48,17 @@ export default function Header({ Connected, setConnected }) {
               <div className="dropdown-menu nav-bg p-3 rounded-4 text-white">
                 <div className="d-flex gap-3 py-2">
                   <div
-                    className="rounded-circle"
+                    className="rounded-circle position-relative"
                     style={{
                       width: "45px",
                       height: "45px",
                       overflow: "hidden",
                     }}
                   >
-                    <img
+                    <Image
                       src={dataSet.profileImage}
                       alt={dataSet.name}
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        objectPosition: "center",
-                      }}
+                      layout="fill"
                     />
                   </div>
                   <div className="d-flex flex-column gap-0">
@@ -69,12 +68,12 @@ export default function Header({ Connected, setConnected }) {
                 </div>
                 <hr className="my-2" />
                 <div className="d-flex flex-column">
-                  <img
-                    src={dataSet.qrCode}
-                    alt="QR code"
-                    className="mx-auto"
+                  <div
+                    className="position-relative mx-auto"
                     style={{ width: "min(140px, 100%)", aspectRatio: "1/1" }}
-                  />
+                  >
+                    <Image layout="fill" src={dataSet.qrCode} alt="QR code" />
+                  </div>
                   <button
                     className="d-flex align-items-center justify-content-center gap-2 text-center body2"
                     onClick={() =>
