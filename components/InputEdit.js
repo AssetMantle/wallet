786af1cc129d90a96751254041846fd72345983d
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-21 20:48:05 +0600

    🛠️💅🏷️added

---
+++ components/InputEdit.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export default function InputEdit({
+  value = "thequickbrownfoxjumpsoverthelazydogfIfthedogr",
+  setValue,
+}) {
+  const [CanEdit, setCanEdit] = useState(false);
+  return (
+    <div
+      className={`p-3 py-2 d-flex rounded-2 gap-2 ${CanEdit ? "am-input" : ""}`}
+    >
+      {CanEdit ? (
+        <input
+          className="bg-t"
+          type="text"
+          value={value}
+          placeholder="Enter address"
+          style={{ flex: "1", border: "none", outline: "none" }}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      ) : (
+        <p className="flex-grow-1">{value}</p>
+      )}
+      <button
+        className="bg-gray-800 p-1 px-2 text-primary"
+        onClick={() => setCanEdit(!CanEdit)}
+      >
+        {CanEdit ? "done" : "edit"}
+      </button>
+    </div>
+  );
+}
---
+++ pages/interchain.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { SlReload } from "react-icons/sl";
+import ICFormETH from "../components/ICFormETH";
+import ICForm from "../components/ICFormETH";
+import ICFormPolygon from "../components/ICFormPolygon";
+
+export default function Interchain() {
+  const tabs = [
+    { name: "Eth", href: "#eth" },
+    { name: "Polygon", href: "#polygon" },
+  ];
+
+  const [Tab, setTab] = useState(0);
+
+  const handleReload = () => {};
+
+  return (
+    <section className="rounded-5 p-4 bg-gray-800 width-100 d-flex flex-column gap-3 transitionAll">
+      <nav className="d-flex align-items-center justify-content-between gap-3">
+        <div className="d-flex gap-3 align-items-center">
+          {React.Children.toArray(
+            tabs.map((tab, index) => (
+              <button
+                className={`am-link ${Tab === index ? "" : "text-white"} body2`}
+                onClick={() => setTab(index)}
+              >
+                {tab.name}
+              </button>
+            ))
+          )}
+        </div>
+        <button className="body2 text-primary" onClick={() => handleReload()}>
+          <SlReload />
+        </button>
+      </nav>
+      {
+        {
+          0: <ICFormETH />,
+          1: <ICFormPolygon />,
+        }[Tab]
+      }
+    </section>
+  );
+}
