4a5bcab6813e582b91771100f58abc67bbd8d8ab
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-24 18:06:48 +0600

    🛠️updated

--- views/Balance.js
+++ views/Balance.js
@@ -3,8 +3,9 @@ import { BsInfoCircle } from "react-icons/bs";
 
 export default function Balance() {
   const dataSet = {
-    totalBalance: "0000.23000",
+    availableBalance: "1213",
     price: "0.312812",
+    totalBalance: "0000.23000",
   };
   return (
     <section className="rounded-5 p-4 bg-gray-800 width-100 d-flex flex-column gap-3">
@@ -12,15 +13,20 @@ export default function Balance() {
       <div className="nav-bg p-3 rounded-4 gap-3">
         <div className="d-flex flex-column gap-2">
           <p className="caption d-flex gap-2 align-items-center">
-            Total{" "}
+            Available{" "}
             <span>
               <BsInfoCircle />
             </span>
           </p>
-          <p className="caption">{dataSet.totalBalance} $MNTL</p>
-          {/* <p className="caption">
-            {Number(dataSet.totalBalance) * Number(dataSet.price)} $USD
-          </p> */}
+          <p className="caption">
+            {Number(dataSet.availableBalance).toFixed(4)} $MNTL
+          </p>
+          <p className="caption">
+            {(Number(dataSet.availableBalance) * Number(dataSet.price)).toFixed(
+              4
+            )}{" "}
+            USD
+          </p>
         </div>
       </div>
       <div className="nav-bg p-3 rounded-4 gap-3">
@@ -28,16 +34,18 @@ export default function Balance() {
           <p className="caption d-flex gap-2 align-items-center">
             Current Price per $MNTL
           </p>
-          <p className="caption">{dataSet.price} USD</p>
+          <p className="caption">{Number(dataSet.price).toFixed(4)} USD</p>
         </div>
       </div>
       <div className="nav-bg p-3 rounded-4 gap-3">
         <div className="d-flex flex-column gap-2">
-          <p className="caption d-flex gap-2 align-items-center">
-            Current Value
+          <p className="caption d-flex gap-2 align-items-center">Total:</p>
+          <p className="caption">
+            {Number(dataSet.totalBalance).toFixed(4)} $MNTL
           </p>
           <p className="caption">
-            {Number(dataSet.totalBalance) * Number(dataSet.price)} USD
+            {(Number(dataSet.totalBalance) * Number(dataSet.price)).toFixed(4)}{" "}
+            USD
           </p>
         </div>
       </div>
--- views/Portfolio.js
+++ views/Portfolio.js
@@ -4,7 +4,6 @@ import { BsInfoCircle } from "react-icons/bs";
 export default function Portfolio() {
   const dataSet = {
     vesting: "0000.23000",
-    transferable: "0000.23000",
     delegable: "0000.23000",
     usd: "0.83",
   };
@@ -22,20 +21,6 @@ export default function Portfolio() {
           <p className="caption">{dataSet.vesting} $MNTL</p>
           <p className="caption">
             {Number(dataSet.vesting) * Number(dataSet.usd)} USD
-          </p>
-        </div>
-      </div>
-      <div className="nav-bg p-3 rounded-4 gap-3">
-        <div className="d-flex flex-column gap-2">
-          <p className="caption d-flex gap-2 align-items-center">
-            Transferable{" "}
-            <span>
-              <BsInfoCircle />
-            </span>
-          </p>
-          <p className="caption">{dataSet.transferable} $MNTL</p>
-          <p className="caption">
-            {Number(dataSet.transferable) * Number(dataSet.usd)} USD
           </p>
         </div>
       </div>
