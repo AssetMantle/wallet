485b6c16667cfefd5bd4e6c373e1be3b8b483861
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-12-06 19:19:42 +0600

    💅🛠️updated

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-export default function Tooltip({ title, description }) {
+export default function Tooltip({ title, description, ...others }) {
   return (
     <div className="position-relative am-tooltip">
       {title}
-      <p className="position-absolute am-tooltip-d caption2 bg-gray-800 p-2 rounded-2">
-        {description}
-      </p>
+      {description && (
+        <p
+          className="position-absolute am-tooltip-d caption2 bg-gray-800 p-2 rounded-2"
+          {...others}
+        >
+          {description}
+        </p>
+      )}
     </div>
   );
 }
