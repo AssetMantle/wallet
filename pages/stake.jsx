b031486ed81dbc4e317e81957eb3b3a6973488e4
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-12-12 12:24:39 +0530

    added functionality to fetch validator avatars

--- pages/stake.jsx
+++ pages/stake.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import {
   useTotalDelegations,
   useDelegatedValidators,
@@ -7,8 +8,10 @@ import {
 import StakedToken from "../views/StakedToken";
 
 const Stake = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [delegated, setDelegated] = useState(false);
   const [selectedValidator, setSelectedValidator] = useState([]);
+  const [validatorAvatar, setValidatorAvatar] = useState([]);
   const {
     delegatedValidators,
     totalDelegatedAmount,
@@ -25,10 +28,66 @@ const Stake = () => {
     }
   });
 
+  const fetchIcons = async () => {
+    if (allValidators?.length != 1) {
+      let allValidatorUrls = allValidators.map(
+        (validator) =>
+          `https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=${validator?.description?.identity}&fields=pictures`
+      );
+      let limitValue = 10;
+
+      const singleFetch = async (url) => {
+        try {
+          const res = await fetch(url);
+          const data = await res.json();
+          if ("them" in data) {
+            const img = data?.them[0]?.pictures?.primary?.url || "";
+            return img;
+          }
+          return "";
+        } catch (error) {
+          throw error;
+        }
+      };
+
+      const multifetch = async (urls) => {
+        if (urls?.length > 0) {
+          return Promise.all(urls.map(singleFetch));
+        }
+        return [];
+      };
+
+      const updateChunk = async (startIndex, allValidatorsUrls) => {
+        try {
+          let slicedArray = allValidatorUrls.slice(
+            startIndex,
+            startIndex + limitValue
+          );
+          let urlChunkArray = await multifetch(slicedArray);
+          setValidatorAvatar((pre) => [...pre, ...urlChunkArray]);
+          if (startIndex < allValidatorUrls.length) {
+            await updateChunk(startIndex + limitValue, allValidatorUrls);
+          }
+        } catch (error) {
+          return [];
+        }
+      };
+      await updateChunk(0, allValidatorUrls);
+    }
+  };
+
   const totalTokens = validatorsArray.reduce(
     (accumulator, currentValue) => accumulator + parseInt(currentValue.tokens),
     0
   );
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      await fetchIcons();
+      setIsLoading(false);
+    })();
+  }, [allValidators?.length]);
 
   return (
     <section className="row">
@@ -53,6 +112,9 @@ const Stake = () => {
                 </th>
                 <th className="text-white" scope="col">
                   Rank
+                </th>
+                <th className="text-white" scope="col">
+                  Avatar
                 </th>
                 <th className="text-white" scope="col">
                   Validator Name
@@ -90,11 +152,6 @@ const Stake = () => {
                       </td>
                       <td className="text-white">{index + 1}</td>
                       <td className="text-white">
-                        {/* <img
-                    alt="validator-logo"
-                    // src={`https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=${item?.description?.identity}&fields=pictures`}
-                    src={()=}
-                  ></img> */}
                         {item?.description?.moniker}
                       </td>
                       <td className="text-white">
@@ -131,11 +188,13 @@ const Stake = () => {
                       </td>
                       <td className="text-white">{index + 1}</td>
                       <td className="text-white">
-                        {/* <img
-                    alt="validator-logo"
-                    // src={`https://keybase.io/_/api/1.0/user/lookup.json?key_suffix=${item?.description?.identity}&fields=pictures`}
-                    src={()=}
-                  ></img> */}
+                        {isLoading ? (
+                          "load.."
+                        ) : (
+                          <img src={validatorAvatar[index]} />
+                        )}
+                      </td>
+                      <td className="text-white">
                         {item?.description?.moniker}
                       </td>
                       <td className="text-white">
