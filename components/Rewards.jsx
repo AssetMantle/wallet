369ba2a104cac80fe27089972fc521dfc6a96420
chudasamajd <68367236+chudasamajd@users.noreplay.github.com>
2022-12-15 19:44:40 +0530

    feat: claim rewards modal

--- components/Rewards.jsx
+++ components/Rewards.jsx
@@ -6,7 +6,13 @@ import {
   placeholderRewards,
 } from "../config";
 import { sendRewards } from "../data";
-import { fromDenom, useTotalRewards, useMntlUsd } from "../data/swrStore";
+import {
+  fromDenom,
+  useTotalRewards,
+  useMntlUsd,
+  useDelegatedValidators,
+  useAllValidators,
+} from "../data/swrStore";
 import { useWallet } from "@cosmos-kit/react";
 
 const denomDisplay = chainSymbol;
@@ -21,11 +27,19 @@ const dataObject = {
 
 const Rewards = ({ selectedValidator }) => {
   const walletManager = useWallet();
+  const {
+    delegatedValidators,
+    totalDelegatedAmount,
+    isLoadingDelegatedAmount,
+    errorDelegatedAmount,
+  } = useDelegatedValidators();
   const { getSigningStargateClient, address, status } = walletManager;
 
   const { allRewards, rewardsArray, isLoadingRewards, errorRewards } =
     useTotalRewards();
   const { mntlUsdValue, errorMntlUsdValue } = useMntlUsd();
+  const { allValidators, isLoadingValidators, errorValidators } =
+    useAllValidators();
 
   const selectedRewards = rewardsArray
     ?.filter((rewardObject) =>
@@ -82,9 +96,138 @@ const Rewards = ({ selectedValidator }) => {
           {rewardsInUSDDisplay}&nbsp;{"$USD"}
         </p>
         <div className="d-flex justify-content-end">
-          <button onClick={handleClaim} className="am-link text-start">
-            <i className="text-primary bi bi-box-arrow-in-down"></i>Claim
-          </button>
+          {selectedValidator?.length > 5 ? null : (
+            <button
+              className="am-link text-start"
+              data-bs-toggle="modal"
+              data-bs-target="#claimRewardsModal"
+            >
+              <i className="text-primary bi bi-box-arrow-in-down"></i>Claim
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div
+        className="modal "
+        tabIndex="-1"
+        role="dialog"
+        id="claimRewardsModal"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 class="modal-title">Claim Rewards</h5>
+              <button
+                type="button"
+                class="btn-close primary"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body p-4 d-flex flex-column text-white">
+              <h6>Total Available $MNTL rewards:</h6>
+              <p>3 $MNTL</p>
+              <p>Selected Validator</p>
+              <table className="table nav-bg">
+                <thead>
+                  <tr>
+                    <th className="text-white" scope="col">
+                      Validator Name
+                    </th>
+                    <th className="text-white" scope="col">
+                      Commission
+                    </th>
+                    <th className="text-white" scope="col">
+                      Delegated Amount
+                    </th>
+                    <th className="text-white" scope="col">
+                      Claimable Rewards
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {selectedValidator.length ? (
+                    delegatedValidators?.filter((item) =>
+                      selectedValidator.includes(item?.operator_address)
+                    ).length ? (
+                      delegatedValidators
+                        ?.filter((item) =>
+                          selectedValidator.includes(item?.operator_address)
+                        )
+                        .map((item, index) => (
+                          <tr key={index}>
+                            <td className="text-white">
+                              {item?.description?.moniker}
+                            </td>
+                            <td className="text-white">
+                              {item?.commission?.commission_rates?.rate * 100}%
+                            </td>
+                            <td className="text-white">
+                              {item?.tokens / 1000000}
+                            </td>
+                            <td className="text-white">
+                              {rewardsArray?.find(
+                                (element) =>
+                                  element?.validator_address ===
+                                  item?.operator_address
+                              )?.reward[0].amount / 1000000}
+                            </td>
+                          </tr>
+                        ))
+                    ) : (
+                      <tr>
+                        <td className="text-white text-center" colSpan={4}>
+                          None of the selected validators have been delegated to
+                        </td>
+                      </tr>
+                    )
+                  ) : delegatedValidators?.length ? (
+                    delegatedValidators?.map((item, index) => (
+                      <tr key={index}>
+                        <td className="text-white">
+                          {item?.description?.moniker}
+                        </td>
+                        <td className="text-white">
+                          {item?.commission?.commission_rates?.rate * 100}%
+                        </td>
+                        <td className="text-white">{item?.tokens / 1000000}</td>
+                        <td className="text-white">
+                          {rewardsArray?.find(
+                            (element) =>
+                              element?.validator_address ===
+                              item?.operator_address
+                          )?.reward[0].amount / 1000000}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td className="text-white text-center" colSpan={4}>
+                        No Record Found!!
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+              <h6>Current wallet adress for claiming staking rewards:</h6>
+              <p>mantle10x0k7tfhd4hm4hgasfuyg689khb34w4a6kbd6v2v</p>
+              <div className="d-flex gap-2">
+                <p>
+                  Want to claim your staking rewards to another wallet address?
+                </p>
+                <a href="#">Setup address</a>
+              </div>
+              <div className="d-flex justify-content-end">
+                <button
+                  className="btn btn-primary w-25 text-right rounded-5"
+                  onClick={handleClaim}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
