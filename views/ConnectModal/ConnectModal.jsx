86b6c3c50b3bd8c180e959aa5d9ac3d60faeeffa
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-05 23:21:43 +0600

    💅made connect modal

--- views/ConnectModal/ConnectModal.jsx
+++ views/ConnectModal/ConnectModal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import ModalContainer from "../../components/ModalContainer";
 
 const ConnectModal = ({ isOpen, setOpen, walletRepo, theme }) => {
   console.log({ isOpen, setOpen, walletRepo, theme });
@@ -27,82 +28,92 @@ const ConnectModal = ({ isOpen, setOpen, walletRepo, theme }) => {
   }
 
   return (
-    isOpen && (
-      <div className="bg-gray-800 p-4 rounded-4 w-100 my-auto">
-        <div className="d-flex align-items-center justify-content-between ">
-          <h1 className="body1 text-primary">Connect Wallet</h1>
-          <button className="btn text-primary body1" onClick={handleCloseModal}>
-            <span className="text-primary">
-              <IoCloseSharp />
-            </span>
-          </button>
-        </div>
-        <div className="text-white body2 my-1 text-center">Connect With</div>
-        <p className="text-white-200 caption my-1 text-center">
-          Connect your wallet using any of the options below
-        </p>
-        <div className="d-flex flex-column gap-3 mt-5">
-          <h2 className="caption text-white">Connect with existing Wallet</h2>
-          <div className="d-flex gap-2 flex-wrap">
-            {walletRepo?.wallets.map(({ _walletInfo, connect }, index) => (
+    // isOpen && (
+    <>
+      <ModalContainer active={isOpen}>
+        <div className="bg-gray-800 p-4 rounded-4 w-100 my-auto">
+          <div className="d-flex align-items-center justify-content-between ">
+            <h1 className="body1 text-primary">Connect Wallet</h1>
+            <button
+              className="btn text-primary body1"
+              onClick={handleCloseModal}
+            >
+              <span className="text-primary">
+                <IoCloseSharp />
+              </span>
+            </button>
+          </div>
+          <div className="text-white body2 my-1 text-center">Connect With</div>
+          <p className="text-white-200 caption my-1 text-center">
+            Connect your wallet using any of the options below
+          </p>
+          <div className="d-flex flex-column gap-3 mt-5">
+            <h2 className="caption text-white">Connect with existing Wallet</h2>
+            <div className="d-flex gap-2 flex-wrap">
+              {walletRepo?.wallets.map(({ _walletInfo, connect }, index) => (
+                <button
+                  className="d-flex align-items-center gap-1 button-secondary py-2 px-4 rounded-2"
+                  key={index}
+                  onClick={async () => {
+                    await connect();
+                    setOpen(false);
+                  }}
+                >
+                  {_walletInfo?.prettyName}
+                </button>
+              ))}
+            </div>
+            <hr className="my-2" />
+            <h2 className="caption text-white">Connect with Ledger</h2>
+            <div className="d-flex">
               <button
                 className="d-flex align-items-center gap-1 button-secondary py-2 px-4 rounded-2"
-                key={index}
-                onClick={async () => await connect()}
+                onClick={handleLedger}
               >
-                {_walletInfo?.prettyName}
+                <span className="btn-label">
+                  <i className="bi bi-hdd-fill"></i>
+                </span>
+                Disconnect
               </button>
-            ))}
-          </div>
-          <hr className="my-2" />
-          <h2 className="caption text-white">Connect with Ledger</h2>
-          <div className="d-flex">
-            <button
-              className="d-flex align-items-center gap-1 button-secondary py-2 px-4 rounded-2"
-              onClick={handleLedger}
-            >
-              <span className="btn-label">
-                <i className="bi bi-hdd-fill"></i>
-              </span>
-              Disconnect
-            </button>
-          </div>
-          <hr className="my-2" />
-          <h2 className="caption text-white">Connect with Keystore</h2>
-          <div className="d-flex">
-            <button
-              className="d-flex align-items-center gap-1 button-secondary py-2 px-4 rounded-2"
-              onClick={handleKeystore}
-            >
-              <span className="btn-label">
-                <i className="bi bi-hdd-fill"></i>
-              </span>
-              Keystore
-            </button>
-          </div>
-          <div className="d-flex align-items-center gap-2 body2">
-            <hr className="divider" />
-            Or
-            <hr className="divider" />
-          </div>
-          <div className="d-flex flex-column">
-            <div className="text-white body2 text-center">
-              Generate Only Mode
             </div>
-            <p className="text-white-200 caption text-center">
-              Use this option to view only and generate .json for transactions
-            </p>
+            <hr className="my-2" />
+            <h2 className="caption text-white">Connect with Keystore</h2>
+            <div className="d-flex">
+              <button
+                className="d-flex align-items-center gap-1 button-secondary py-2 px-4 rounded-2"
+                onClick={handleKeystore}
+              >
+                <span className="btn-label">
+                  <i className="bi bi-hdd-fill"></i>
+                </span>
+                Keystore
+              </button>
+            </div>
+            <div className="d-flex align-items-center gap-2 body2">
+              <hr className="divider" />
+              Or
+              <hr className="divider" />
+            </div>
+            <div className="d-flex flex-column">
+              <div className="text-white body2 text-center">
+                Generate Only Mode
+              </div>
+              <p className="text-white-200 caption text-center">
+                Use this option to view only and generate .json for transactions
+              </p>
+            </div>
+            <input
+              type="text"
+              className="am-input border-color-white bg-transparent px-4 py-1 rounded-2 mt-3"
+              placeholder="Input your wallet address"
+              value={generatedAddress}
+              onChange={handleChangeGenAddress}
+            />
           </div>
-          <input
-            type="text"
-            className="am-input border-color-white bg-transparent px-4 py-1 rounded-2 mt-3"
-            placeholder="Input your wallet address"
-            value={generatedAddress}
-            onChange={handleChangeGenAddress}
-          />
         </div>
-      </div>
-    )
+      </ModalContainer>
+    </>
+    // )
   );
 };
 
