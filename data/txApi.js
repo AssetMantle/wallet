fe82f66cef5e298ab72b43ad103f5e69aa59518d
Kamlesh P <kamlesh@assetmantle.one>
2023-01-11 15:41:55 +0530

    removed mobile wallet connections from cosmos-kit

--- data/txApi.js
+++ data/txApi.js
@@ -384,3 +384,64 @@ export const sendRewardsBatched = async (
     return { response: null, error };
   }
 };
+
+export const sendIbcTransaction = async (
+  proposalId,
+  voter,
+  option,
+  memo,
+  {
+    getSigningStargateClient,
+    chainName = defaultChainName,
+    chainDenom = defaultChainDenom,
+  }
+) => {
+  try {
+    // get the chain assets for the specified chain
+    const chainassets = assets.find((chain) => chain.chain_name === chainName);
+    // get the coin data from the chain assets data
+    const coin = chainassets.assets.find((asset) => asset.base === chainDenom);
+    // // get the amount in denom terms
+    // const amountInDenom = toChainDenom(amount, chainName, chainDenom);
+    // // populate the optional argument fromAddress
+    const fromAddress = voter;
+    // // initialize stargate client and create txn
+    const stargateClient = await getSigningStargateClient();
+    if (!stargateClient || !fromAddress) {
+      throw new error("stargateClient or from address undefined");
+    }
+
+    // create a message template from the composer
+    const { vote } = cosmos.gov.v1beta1.MessageComposer.withTypeUrl;
+
+    const msg = vote({
+      proposalId,
+      voter,
+      option,
+    });
+    // populate the fee data
+    const fee = {
+      amount: [
+        {
+          denom: "umntl",
+          amount: "2000",
+        },
+      ],
+      gas: "86364",
+    };
+    // use the stargate client to dispatch the transaction
+    const response = await stargateClient.signAndBroadcast(
+      voter,
+      [msg],
+      fee,
+      memo
+    );
+    console.log("msg: ", msg, " amount: ", amountInDenom);
+    return { response, error: null };
+  } catch (error) {
+    console.error("Error during transaction: ", error?.message);
+    return { response: null, error };
+  }
+};
+
+
