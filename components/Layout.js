1dcd312d9f2c1317c748d1ac8214a8292472bf0f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-17 14:03:54 +0600

    🛠️🏷️💅📕banner component added

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Balance from "../views/Balance";
 import Header from "../views/Header";
+import Banner from "./Banner";
 // import Vesting from "../views/Vesting";
 import ScrollableSectionContainer from "./ScrollableSectionContainer";
 
 export default function Layout({ children }) {
   return (
     <div className="am_app_container">
+      <Banner />
       <Header />
       <main className="container-xxl pt-4 h-100">
         <div className="row px-2 position-relative h-100">
