c31278106143246d517b1cbb29e73223ca615034
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-19 16:52:44 +0600

    🛠️🦄💅qr code dummy updated

--- components/DisconnecedRecieve.jsx
+++ components/DisconnecedRecieve.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { placeholderAddress } from "../data";
 
@@ -11,21 +12,13 @@ const DisconnecedRecieve = () => {
       </nav>
       <div className="nav-bg rounded-4 d-flex flex-column p-3 gap-2 align-items-center justify-content-center">
         <div
+          className="text-gray position-relative d-flex justify-content-center"
           style={{
             width: "min(140px, 100%)",
             aspectRatio: "1/1",
-            position: "relative",
-            display: "flex",
-            justifyContent: "center",
           }}
         >
-          <i
-            className="text-gray bi bi-qr-code-scan"
-            style={{
-              fontSize: "124px",
-              height: "124px",
-            }}
-          ></i>
+          <Image src="/qr-code.svg" layout="fill" alt="qr code place holder" />
         </div>
         <h4 className="body2 text-primary">Wallet Address</h4>
         <div className="d-flex align-items-center justify-content-center gap-2 text-gray text-center caption2 pt-1">
