b9ff9b0a2f682540f65815fda5d641570079ace5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-27 17:25:02 +0600

    📕💅🛠️🏷️trade page basic setup

--- pages/trade.jsx
+++ pages/trade.jsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import Head from "next/head";
 
-export default function trade() {
+export default function Trade() {
+  const [searchValue, setSearchValue] = useState();
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <Head>
+        <title>Trade | MantleWallet</title>
+      </Head>
+      <section className="row h-100">
+        <div className="col-12 col-lg-8 h-100">
+          <div
+            className="bg-gray-800 p-3 rounded-4 d-flex flex-column gap-2"
+            style={{ maxHeight: "90%" }}
+          >
+            <div className="d-flex align-items-center justify-content-between w-100">
+              <h1 className="card-title body1 text-primary my-auto">
+                Exchanges
+              </h1>
+            </div>
+            <div
+              className="d-flex flex-column w-100 nav-bg p-2 rounded-4 flex-grow-1"
+              style={{ maxHeight: "88%" }}
+            >
+              <div
+                className="d-flex align-items-center gap-3 w-100 p-2 border-color-white"
+                style={{ borderBottom: "1px solid" }}
+              >
+                <div
+                  className="d-flex gap-2 am-input border-color-white rounded-3 py-1 px-3 align-items-center"
+                  style={{ flex: "1" }}
+                >
+                  <span
+                    className="input-group-text bg-t p-0 h-100"
+                    id="basic-addon1"
+                    style={{ border: "none" }}
+                  >
+                    <i className="bi bi-search text-white"></i>
+                  </span>
+                  <input
+                    type="text"
+                    className="am-input bg-t p-1 w-100 h-100"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    style={{ border: "none" }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-4">
+          <div className="rounded-4 p-3 my-2 bg-gray-800 width-100 d-flex flex-column ">
+            <p>
+              Instructional Copy. Lorem Ipsum Dolor Sit ametLorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Aliquam pulvinar vitae
+              massa in egestas. Vivamus tincidunt leo nulla.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
