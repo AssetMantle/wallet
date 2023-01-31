5b4d62e609cb908b447aafdb1780718bd9443a22
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-31 17:07:21 +0600

    📕🛠️💅added custom delegate modal
    Signed-off-by: Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>

--- views/StakedToken.js
+++ views/StakedToken.js
@@ -1,6 +1,7 @@
 import { useChain } from "@cosmos-kit/react";
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import Delegations from "../components/Delegations";
+import ModalContainer from "../components/ModalContainer";
 import Rewards from "../components/Rewards";
 import Unbonded from "../components/Unbonded";
 import { defaultChainName, defaultChainSymbol } from "../config";
@@ -17,6 +18,8 @@ export default function StakedToken({
   const { availableBalance } = useAvailableBalance();
   const walletManager = useChain(defaultChainName);
   const { getSigningStargateClient, address, status, wallet } = walletManager;
+
+  const [DelegateModal, setDelegateModal] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -69,14 +72,15 @@ export default function StakedToken({
               className="button-primary text-center px-3 py-2"
               style={{ maxWidth: "100%" }}
               disabled={isSubmitDisabled}
-              onClick={() =>
+              onClick={() => {
                 stakeDispatch({
                   type: "SET_DELEGATION_ADDRESS",
                   payload: stakeState?.selectedValidators[0],
-                })
-              }
-              data-bs-toggle="modal"
-              data-bs-target="#delegateModal"
+                });
+                setDelegateModal(true);
+              }}
+              // data-bs-toggle="modal"
+              // data-bs-target="#delegateModal"
             >
               Delegate
             </button>
@@ -91,7 +95,92 @@ export default function StakedToken({
           ) : null}
         </div>
       </section>
-      <div className="modal " tabIndex="-1" role="dialog" id="delegateModal">
+      <ModalContainer active={DelegateModal}>
+        <div className="d-flex flex-column bg-gray-700 m-auto p-4 rounded-3 w-100">
+          <div className="d-flex align-items-center justify-content-between">
+            <h5 className="body2 text-primary d-flex align-items-center gap-2">
+              <button
+                className="btn-close primary bg-t"
+                onClick={() => setDelegateModal(false)}
+                style={{ background: "none" }}
+              >
+                <span className="text-primary">
+                  <i className="bi bi-chevron-left" />
+                </span>
+              </button>
+              Delegate
+            </h5>
+            <button
+              className="btn-close primary bg-t"
+              onClick={() => setDelegateModal(false)}
+              style={{ background: "none" }}
+            >
+              <span className="text-primary">
+                <i className="bi bi-x-lg" />
+              </span>
+            </button>
+          </div>
+          <div className="py-4 text-center d-flex flex-column">
+            <div className="d-flex my-2 justify-content-between">
+              <label htmlFor="delegationAmount caption text-gray">
+                Delegation Amount
+              </label>{" "}
+              <small className="text-gray caption2">
+                Balance : {fromDenom(availableBalance).toString()}&nbsp;
+                {defaultChainSymbol}
+              </small>
+            </div>
+            <div>
+              <div className="p-3 border-white py-2 d-flex rounded-2 gap-2 am-input">
+                <input
+                  className="bg-t"
+                  id="delegationAmount"
+                  style={{ flex: "1", border: "none", outline: "none" }}
+                  type="text"
+                  value={stakeState?.delegationAmount}
+                  placeholder="Enter Delegation Amount"
+                  onChange={(e) =>
+                    stakeDispatch({
+                      type: "CHANGE_DELEGATION_AMOUNT",
+                      payload: e.target.value,
+                    })
+                  }
+                ></input>
+                <button
+                  onClick={() =>
+                    stakeDispatch({
+                      type: "SET_MAX_DELEGATION_AMOUNT",
+                    })
+                  }
+                  className="text-primary"
+                >
+                  Max
+                </button>
+              </div>
+              <small
+                id="amountInputErrorMsg"
+                className="form-text text-danger d-flex align-items-center gap-1"
+              >
+                {stakeState?.errorMessages?.transferAmountErrorMsg && (
+                  <i className="bi bi-info-circle" />
+                )}{" "}
+                {stakeState?.errorMessages?.transferAmountErrorMsg}
+              </small>
+            </div>
+          </div>
+          <div className="d-flex align-items-center gap-2 justify-content-end">
+            <button
+              disabled={!isObjEmpty(stakeState?.errorMessages)}
+              type="button"
+              className="button-primary px-5 py-2"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </ModalContainer>
+      {/* <div className="modal " tabIndex="-1" role="dialog" id="delegateModal">
         <div className="modal-dialog modal-dialog-centered" role="document">
           <div className="modal-content">
             <div className="modal-header">
@@ -185,7 +274,7 @@ export default function StakedToken({
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* <TransactionManifestModal
         id="stakeTransactionManifestModal"
