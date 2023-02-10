bd63f7434c5fe82d3964fb7d1cfc74d53cb58f54
Jathin Kotian <jathin@assetmantle.one>
2023-02-10 23:34:19 +0530

    reverted to old table in trade

--- pages/trade.jsx
+++ pages/trade.jsx
@@ -1,39 +1,122 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import { useTrade } from "../data";
-import Table from "../components/Table";
 
 export default function Trade() {
   const [searchValue, setSearchValue] = useState("");
   const { allTrades, isLoadingTrades, errorTrades } = useTrade();
-  const columns = [
+  console.log(allTrades?.tickers);
+  const tradeInfo = [
     {
-      Header: "",
-      accessor: "logo",
-      Cell: (tableProps) => (
-        <img src={tableProps.row.original.logo} width={20} alt="logo" />
-      ),
+      logo: "/osmosis.png",
+      name: "Osmosis",
+      target_coin_id: "osmosis",
+      pair: "MNTL/OSMO",
+      subTitle: "",
+      url: "https://app.osmosis.zone/?from=OSMO&to=MNTL",
+      target: "_blank",
+      row: 2,
+      col: 1,
     },
     {
-      Header: "Exchange Name",
-      accessor: "exchangeName",
-      Cell: (tableProps) => (
-        <>
-          <a
-            href={tableProps.row.original.url}
-            target="_blank"
-            width={20}
-            rel="noreferrer"
-          >
-            {tableProps.row.original.exchangeName}
-          </a>{" "}
-          <i className="bi bi-arrow-up-right"></i>
-        </>
-      ),
+      logo: "/osmosis.png",
+      name: "Osmosis",
+      target_coin_id: "axlusdc",
+      pair: "MNTL/AXLUSDC",
+      subTitle: "",
+      url: "https://app.osmosis.zone/?from=USDC&to=MNTL",
+      target: "_blank",
+      row: 2,
+      col: 1,
     },
-    { Header: "Trade Pair", accessor: "tradePair" },
-    { Header: "Price", accessor: "price" },
-    { Header: "Volume", accessor: "volume" },
+    {
+      logo: "/osmosis.png",
+      name: "Osmosis",
+      target_coin_id: "assetmantle",
+      pair: "ATOM/MNTL",
+      subTitle: "",
+      url: "https://app.osmosis.zone/?from=ATOM&to=MNTL",
+      target: "_blank",
+      row: 2,
+      col: 1,
+    },
+    {
+      logo: "/Uniswap.png",
+      name: "Uniswap",
+      pair: "MNTL/ERC20",
+
+      subTitle: "(ETH Pool)",
+      url: "https://app.uniswap.org/#/swap?theme=dark&inputCurrency=ETH&outputCurrency=0x2c4f1df9c7de0c59778936c9b145ff56813f3295",
+      target: "_blank",
+      row: 4,
+      col: 5,
+    },
+    {
+      logo: "/quickswap.webp",
+      name: "Quickswap",
+      pair: "MNTL/USDC",
+      target_coin_id: "usd-coin",
+      subTitle: "",
+      url: "https://quickswap.exchange/#/swap?swapIndex=0&currency0=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174&currency1=0x38A536A31bA4d8C1Bcca016AbBf786ecD25877E8",
+      target: "_blank",
+      row: 2,
+      col: 1,
+    },
+    {
+      logo: "/LBank.webp",
+      name: "LBank",
+      pair: "MNTL/USDT",
+      target_coin_id: "tether",
+      subTitle: "",
+      url: "https://www.lbank.info/exchange/mntl/usdt",
+      target: "_blank",
+      row: 2,
+      col: 1,
+    },
+    {
+      logo: "/MEXC.webp",
+      name: "MEXC Global",
+      pair: "MNTL/USDT",
+      target_coin_id: "tether",
+      subTitle: "",
+      url: "https://www.mexc.com/exchange/MNTL_USDT?inviteCode=1498J",
+      target: "_blank",
+      row: 2,
+      col: 1,
+    },
+    {
+      logo: "/quickswap.webp",
+      name: "Quickswap",
+      pair: "MNTL/VERSA",
+      target_coin_id: "versagames",
+      subTitle: "",
+      url: "https://quickswap.exchange/#/swap?inputCurrency=0x8497842420cfdbc97896c2353d75d89fc8d5be5d&outputCurrency=0x38a536a31ba4d8c1bcca016abbf786ecd25877e8&swapIndex=0",
+      target: "_blank",
+      row: 2,
+      col: 1,
+    },
+    // {
+    //   logo: "/osmosis.png",
+    //   name: "P2B",
+    //   pair: "MNTL/USDT",
+
+    //   subTitle: "(USDC Pool)",
+    //   url: "https://p2pb2b.com/trade/MNTL_USDT/",
+    //   target: "_blank",
+    //   row: 1,
+    //   col: 3,
+    // },
+    // {
+    //   logo: "/osmosis.png",
+    //   name: "Coinsbit",
+    //   pair: "MNTL/USDT",
+
+    //   subTitle: "(USDC Pool)",
+    //   url: "https://coinsbit.io/trade/MNTL_USDT",
+    //   target: "_blank",
+    //   row: 1,
+    //   col: 3,
+    // },
   ];
 
   return (
@@ -80,8 +163,7 @@ export default function Trade() {
                 </div>
               </div>
               <div className="w-100 h-100" style={{ overflow: "auto" }}>
-                <Table columns={columns} data={allTrades} />
-                {/* <table
+                <table
                   className="table"
                   style={{ width: "max-content", minWidth: "100%" }}
                 >
@@ -106,12 +188,18 @@ export default function Trade() {
                       </th>
                     </tr>
                   </thead>
-                  <tbody> */}
-                {/* {filteredData
-                      ?.filter((item) =>
-                        item?.name
-                          .toLowerCase()
-                          .includes(searchValue.toLowerCase())
+                  <tbody>
+                    {tradeInfo
+                      ?.filter(
+                        (item) =>
+                          item?.name
+                            .toLowerCase()
+                            .includes(searchValue.toLowerCase()) &&
+                          allTrades?.tickers?.find(
+                            (element) =>
+                              element?.market?.name == item?.name &&
+                              element?.target_coin_id == item?.target_coin_id
+                          )
                       )
                       ?.map((item, index) => (
                         <tr key={index} className="caption2 text-white-300">
@@ -140,12 +228,30 @@ export default function Trade() {
                             </div>
                           </td>
                           <td>{item.pair}</td>
-                          <td>${item?.price}</td>
-                          <td>${item?.volume}</td>
+                          <td>
+                            $
+                            {
+                              allTrades?.tickers?.find(
+                                (ele) =>
+                                  ele?.market?.name == item.name &&
+                                  ele?.target_coin_id == item?.target_coin_id
+                              )?.converted_last?.usd
+                            }
+                          </td>
+                          <td>
+                            $
+                            {
+                              allTrades?.tickers?.find(
+                                (ele) =>
+                                  ele?.market?.name == item.name &&
+                                  ele?.target_coin_id == item?.target_coin_id
+                              )?.converted_volume?.usd
+                            }
+                          </td>
                         </tr>
-                      ))} */}
-                {/* </tbody> */}
-                {/* </table> */}
+                      ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
