11fea2e553e11467d2c94c3f2f001ccfee4b80a2
Jathin Kotian <jathin@assetmantle.one>
2023-02-14 14:45:30 +0530

    created trade table file; made changes to useTrade in queryApi

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,29 +1,86 @@
 import React from "react";
-import { useTable, useSortBy } from "react-table";
+import {
+  useTable,
+  useSortBy,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from "react-table";
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
+
+  return (
+    <div className="d-flex align-items-center gap-3 w-100 p-2">
+      <div
+        className="d-flex gap-2 am-input border-color-white rounded-3 py-1 px-3 align-items-center"
+        style={{ flex: "1" }}
+      >
+        <span
+          className="input-group-text bg-t p-0 h-100"
+          id="basic-addon1"
+          style={{ border: "none" }}
+        >
+          {" "}
+          <i className="bi bi-search text-white"></i>
+        </span>
+        <input
+          className="w-100 bg-t "
+          value={value || ""}
+          onChange={(e) => {
+            setValue(e.target.value);
+            onChange(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
 
 const Table = ({ columns, data }) => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useSortBy
-    );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy
+  );
 
   return (
     <>
-      <table className="table caption2 text-white-300" {...getTableProps()}>
+      <GlobalFilter
+        preGlobalFilteredRows={preGlobalFilteredRows}
+        globalFilter={state.globalFilter}
+        setGlobalFilter={setGlobalFilter}
+      />
+      <table className="table caption2 text-white-300" {...getTableProps}>
         <thead>
           {headerGroups.map((headerGroup, index) => (
-            <tr key={index} {...headerGroup.getHeaderGroupProps()}>
+            <tr key={index} {...headerGroup.getHeaderGroupProps}>
               {headerGroup.headers.map((column, i) => (
                 // Add the sorting props to control sorting. For this example
                 // we can add them into the header props
                 <th
                   key={i}
                   className="text-white"
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  {...column.getHeaderProps(column.getSortByToggleProps)}
                 >
                   {column.render("Header")}
                   {/* Add a sort direction indicator */}
@@ -39,15 +96,15 @@ const Table = ({ columns, data }) => {
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
+        <tbody {...getTableBodyProps}>
           {rows.map((row, i) => {
             prepareRow(row);
             return (
-              <tr key={i} {...row.getRowProps()}>
+              <tr key={i} {...row.getRowProps}>
                 {row.cells.map((cell, index) => {
                   console.log(cell);
                   return (
-                    <td key={index} {...cell.getCellProps()}>
+                    <td key={index} {...cell.getCellProps}>
                       {cell.render("Cell")}
                     </td>
                   );
