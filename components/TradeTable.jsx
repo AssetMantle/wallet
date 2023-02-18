9a4e020f7ab9a75bdbf7080f22b2e7f90cae30e9
Jathin Kotian <jathin@assetmantle.one>
2023-02-18 21:39:40 +0530

    minor fixes

--- components/TradeTable.jsx
+++ components/TradeTable.jsx
@@ -15,7 +15,7 @@ const TradeTable = ({ data }) => {
         Cell: (tableProps) => (
           <div className="d-flex align-items-center justify-content-around">
             <img
-              src={`/trade/${tableProps?.row?.original?.logo?.toLowerCase()}.webp`}
+              src={`/trade/${tableProps?.row?.original?.logo}.webp`}
               width={20}
               alt="logo"
             />
