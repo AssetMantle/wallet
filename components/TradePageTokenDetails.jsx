a02c05ae6f13497aab18f50bab20ff7a72ccb483
Jathin Kotian <jathin@assetmantle.one>
2023-03-29 17:31:22 +0530

    added loading containers and placheolder data to trade page; change values of defaultRefreshInterval and slowRefreshInterval; added cswap static data in trade page

--- components/TradePageTokenDetails.jsx
+++ components/TradePageTokenDetails.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { getBalanceStyle } from "../config";
-import { decimalize } from "../data";
+import { decimalize, useTrade } from "../data";
 
-const TradePageTokenDetails = ({ data }) => {
+const TradePageTokenDetails = () => {
+  const { allTrades, isLoadingTrades, errorTrades } = useTrade();
+  const data = allTrades?.tokenDetails;
+
   return (
     <div className="rounded-4 p-3 bg-gray-800 width-100 d-flex flex-column gap-3">
       <h4 className="body1 text-primary">Token Details</h4>
@@ -12,57 +15,93 @@ const TradePageTokenDetails = ({ data }) => {
             Market Cap
           </p>
         </div>
-        <p className="caption pb-3">${decimalize(data?.marketCap, 2)}</p>
+        {isLoadingTrades ? (
+          <p className="placeholder-glow">
+            <span className="placeholder col-6 w-100"></span>
+          </p>
+        ) : (
+          <p className="caption pb-3">${decimalize(data?.marketCap, 2)}</p>
+        )}
         <div>
           <p className="caption d-flex gap-2 align-items-center text-white-300">
             Circulating Supply
           </p>
         </div>
-        <p className="caption pb-3">
-          {getBalanceStyle(
-            decimalize(data?.circulatingSupply),
-            "caption",
-            "caption2"
-          )}
-        </p>
+        {isLoadingTrades ? (
+          <p className="placeholder-glow">
+            <span className="placeholder col-6 w-100"></span>
+          </p>
+        ) : (
+          <p className="caption pb-3">
+            {getBalanceStyle(
+              decimalize(data?.circulatingSupply),
+              "caption",
+              "caption2"
+            )}
+          </p>
+        )}
         <div>
           <p className="caption d-flex gap-2 align-items-center text-white-300">
             Total Supply
           </p>
         </div>
-        <p className="caption pb-3">
-          {getBalanceStyle(
-            decimalize(data?.totalSupply),
-            "caption",
-            "caption2"
-          )}
-        </p>
+        {isLoadingTrades ? (
+          <p className="placeholder-glow">
+            <span className="placeholder col-6 w-100"></span>
+          </p>
+        ) : (
+          <p className="caption pb-3">
+            {getBalanceStyle(
+              decimalize(data?.totalSupply),
+              "caption",
+              "caption2"
+            )}
+          </p>
+        )}
         <div>
           <p className="caption d-flex gap-2 align-items-center text-white-300">
             Max Supply
           </p>
         </div>
-        <p className="caption pb-3">${decimalize(data?.maxSupply, 0)}</p>
+        {isLoadingTrades ? (
+          <p className="placeholder-glow">
+            <span className="placeholder col-6 w-100"></span>
+          </p>
+        ) : (
+          <p className="caption pb-3">${decimalize(data?.maxSupply, 0)}</p>
+        )}
         <div>
           <p className="caption d-flex gap-2 align-items-center text-white-300">
             24 Hour Trading Volume
           </p>
-          <p className="caption pb-3">
-            $
-            {getBalanceStyle(
-              decimalize(data?.volume, 2),
-              "caption",
-              "caption2"
-            )}
-          </p>
+          {isLoadingTrades ? (
+            <p className="placeholder-glow">
+              <span className="placeholder col-6 w-100"></span>
+            </p>
+          ) : (
+            <p className="caption pb-3">
+              $
+              {getBalanceStyle(
+                decimalize(data?.volume, 2),
+                "caption",
+                "caption2"
+              )}
+            </p>
+          )}
         </div>
         <div>
           <p className="caption d-flex gap-2 align-items-center text-white-300">
             Fully Diluted Valuation
           </p>
-          <p className="caption pb-3">
-            ${decimalize(data?.fullyDilutedValuation, 2)}
-          </p>
+          {isLoadingTrades ? (
+            <p className="placeholder-glow">
+              <span className="placeholder col-6 w-100"></span>
+            </p>
+          ) : (
+            <p className="caption pb-3">
+              ${decimalize(data?.fullyDilutedValuation, 2)}
+            </p>
+          )}
         </div>
       </div>
     </div>
