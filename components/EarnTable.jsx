cf17e0bf16d745835d392e5ba11cda7edbe08655
Jathin Kotian <jathin@assetmantle.one>
2023-04-05 13:56:06 +0530

    Made changes to show static data for some columns in trade and earn tables. Fixed an issue in trade table where uniswap row was breaking

--- components/EarnTable.jsx
+++ components/EarnTable.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { getBalanceStyle } from "../config";
-import { decimalize, useOsmosis, useQuickswap } from "../data";
+import { decimalize, staticEarnData, useOsmosis, useQuickswap } from "../data";
 import Table from "./Table";
 
 const EarnTable = () => {
   const { allOsmosis, isLoadingOsmosis, errorOsmosis } = useOsmosis();
   const { allQuickswap, isLoadingQuickswap, errorQuickswap } = useQuickswap();
   let isLoading = isLoadingOsmosis || isLoadingQuickswap;
-  const fetchedData = isLoading ? [] : [...allOsmosis, ...allQuickswap];
-  console.log(allOsmosis);
+  let isError = errorOsmosis || errorQuickswap;
+  const fetchedData =
+    isLoading || isError ? [] : [...allOsmosis, ...allQuickswap];
+
   const loadingData = [
     { symbol: true, project: true, chains: true, apy: true, tvlUsd: true },
     { symbol: true, project: true, chains: true, apy: true, tvlUsd: true },
@@ -27,6 +29,15 @@ const EarnTable = () => {
     { symbol: true, project: true, chains: true, apy: true, tvlUsd: true },
     { symbol: true, project: true, chains: true, apy: true, tvlUsd: true },
   ];
+
+  const placeHolderData = staticEarnData;
+
+  let earnData = [];
+  if (isLoading || isError) {
+    earnData = placeHolderData;
+  } else {
+    earnData = fetchedData;
+  }
 
   const fetchedColumns = [
     {
@@ -122,65 +133,128 @@ const EarnTable = () => {
     },
   ];
 
-  const loadingColumns = [
+  const nonFetchedColumns = [
     {
       Header: "Name",
       accessor: "symbol",
-      Cell: (tableProps) =>
-        tableProps.row.original.symbol ? (
-          <p className="placeholder-glow">
-            <span className="placeholder col-6"></span>
-          </p>
-        ) : null,
+      Cell: (tableProps) => (
+        <div className="d-flex align-items-center justify-content-around">
+          <a
+            href={tableProps.row.original.url}
+            target="_blank"
+            width={20}
+            rel="noreferrer"
+          >
+            {tableProps.row.original.symbol}
+          </a>{" "}
+          <i className="bi bi-arrow-up-right"></i>
+        </div>
+      ),
     },
     {
       Header: "Category",
       accessor: "project",
-      Cell: (tableProps) =>
-        tableProps.row.original.project ? (
-          <p className="placeholder-glow">
-            <span className="placeholder col-6"></span>
-          </p>
-        ) : null,
+      Cell: (tableProps) => (
+        <div className="d-flex justify-content-start  gap-2">
+          <div
+            className="d-flex position-relative rounded-circle gap-1"
+            style={{
+              width: "20px",
+              aspectRatio: "1/1",
+              marginLeft: "5px",
+            }}
+          >
+            <img
+              alt={tableProps.row.original.logo?.toLowerCase()}
+              className="rounded-circle"
+              layout="fill"
+              src={`/tradePage/${tableProps?.row?.original?.project?.toLowerCase()}.webp`}
+            />
+          </div>
+          <p>{tableProps?.row?.original?.project}</p>
+        </div>
+      ),
     },
     {
       Header: "Chains",
       accessor: "chains",
-      Cell: (tableProps) =>
-        tableProps.row.original.chains ? (
-          <p className="placeholder-glow">
-            <span className="placeholder col-6"></span>
-          </p>
-        ) : null,
+      Cell: (tableProps) => (
+        <div className="d-flex justify-content-center">
+          <div
+            className="d-flex position-relative rounded-circle gap-1"
+            style={{
+              width: "20px",
+              aspectRatio: "1/1",
+            }}
+          >
+            <img
+              alt={tableProps?.row?.original?.logo}
+              className="rounded-circle"
+              layout="fill"
+              src={`/earn/${tableProps?.row?.original?.chain?.toLowerCase()}.svg`}
+            />
+          </div>
+        </div>
+      ),
     },
     {
       Header: "APY",
       accessor: "apy",
-      Cell: (tableProps) =>
-        tableProps.row.original.apy ? (
-          <p className="placeholder-glow">
-            <span className="placeholder col-6"></span>
-          </p>
-        ) : null,
+      Cell: (tableProps) => (
+        <div>
+          {isLoading ? (
+            <p className="placeholder-glow">
+              <span className="placeholder col-6"></span>
+            </p>
+          ) : (
+            <p>
+              {" "}
+              {getBalanceStyle(
+                decimalize(tableProps?.row?.original?.apy, 4),
+                "caption text-white-300",
+                "caption2 text-white-300"
+              )}
+              %
+            </p>
+          )}
+        </div>
+      ),
     },
     {
       Header: "TVL",
       accessor: "tvlUsd",
-      Cell: (tableProps) =>
-        tableProps.row.original.tvlUsd ? (
-          <p className="placeholder-glow">
-            <span className="placeholder col-6"></span>
-          </p>
-        ) : null,
+      Cell: (tableProps) => (
+        <div>
+          {isLoading ? (
+            <p className="placeholder-glow">
+              <span className="placeholder col-6"></span>
+            </p>
+          ) : (
+            <p>
+              {" "}
+              $
+              {getBalanceStyle(
+                decimalize(tableProps?.row?.original?.tvlUsd, 2),
+                "caption text-white-300",
+                "caption2 text-white-300"
+              )}
+            </p>
+          )}
+        </div>
+      ),
     },
   ];
+
+  let columns = [];
+  if (isLoading || isError) {
+    columns = nonFetchedColumns;
+  } else {
+    columns = fetchedColumns;
+  }
+
   return (
     <>
-      {isLoading ? (
-        <Table columns={loadingColumns} data={loadingData} />
-      ) : (
-        <Table columns={fetchedColumns} data={fetchedData} />
-      )}
+      <Table columns={columns} data={earnData} />
     </>
   );
 };
--- components/TradeTable.jsx
+++ components/TradeTable.jsx
@@ -6,194 +6,146 @@ import Table from "./Table";
 
 const TradeTable = () => {
   const { allTrades, isLoadingTrades, errorTrades } = useTrade();
-  const loadingData = [
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-    { exchangeName: true, tradePair: true, price: true, volume: true },
-  ];
 
   const placeHolderData = staticTradeData;
   // const tradeData =React.useMemo(()=>[],[])
   let tradeData = [];
-  if (isLoadingTrades) {
-    tradeData = loadingData;
-  } else if (errorTrades) {
+  if (isLoadingTrades || errorTrades) {
     tradeData = placeHolderData;
   } else {
     tradeData = allTrades?.tradeData;
   }
   const memoizedData = React.useMemo(() => tradeData, [isLoadingTrades]);
-  const fetchedColumns = React.useMemo(
-    () => [
-      {
-        Header: "Market Name",
-        accessor: "exchangeName",
-        Cell: (tableProps) => (
-          <div className="d-flex align-items-center justify-content-around">
-            <img
-              src={`/tradePage/${tableProps?.row?.original?.logo}.webp`}
-              width={20}
-              alt="logo"
-            />
-            <a
-              href={tableProps.row.original.url}
-              target="_blank"
-              width={20}
-              rel="noreferrer"
-            >
-              {tableProps.row.original.exchangeName}
-            </a>{" "}
-            <i className="bi bi-arrow-up-right"></i>
-          </div>
-        ),
-        sortMethod: (a, b) => a - b,
-      },
-      { Header: "Trade Pair", accessor: "tradePair" },
-      {
-        Header: "Price",
-        accessor: "price",
-        Cell: (tableProps) => (
-          <p>
-            $
-            {getBalanceStyle(
-              decimalize(tableProps.row.original.price),
-              "caption text-white-300",
-              "caption2 text-white-300"
-            )}
-          </p>
-        ),
-        sortMethod: (a, b) => parseFloat(a) - parseFloat(b),
-      },
-      {
-        Header: "Volume(24Hour)",
-        accessor: "volume",
-        Cell: (tableProps) => (
-          <p>
-            {getBalanceStyle(
-              decimalize(tableProps.row.original.volume),
-              "caption text-white-300",
-              "caption2 text-white-300"
-            )}
-          </p>
-        ),
-      },
-    ],
-    []
-  );
+  const fetchedColumns = [
+    {
+      Header: "Market Name",
+      accessor: "exchangeName",
+      Cell: (tableProps) => (
+        <div className="d-flex align-items-center justify-content-around">
+          <img
+            src={`/tradePage/${tableProps?.row?.original?.logo}.webp`}
+            width={20}
+            alt="logo"
+          />
+          <a
+            href={tableProps.row.original.url}
+            target="_blank"
+            width={20}
+            rel="noreferrer"
+          >
+            {tableProps.row.original.exchangeName}
+          </a>{" "}
+          <i className="bi bi-arrow-up-right"></i>
+        </div>
+      ),
+      sortMethod: (a, b) => a - b,
+    },
+    { Header: "Trade Pair", accessor: "tradePair" },
+    {
+      Header: "Price",
+      accessor: "price",
+      Cell: (tableProps) => (
+        <p>
+          $
+          {getBalanceStyle(
+            decimalize(tableProps.row.original.price),
+            "caption text-white-300",
+            "caption2 text-white-300"
+          )}
+        </p>
+      ),
+      sortMethod: (a, b) => parseFloat(a) - parseFloat(b),
+    },
+    {
+      Header: "Volume(24Hour)",
+      accessor: "volume",
+      Cell: (tableProps) => (
+        <p>
+          {getBalanceStyle(
+            decimalize(tableProps.row.original.volume),
+            "caption text-white-300",
+            "caption2 text-white-300"
+          )}
+        </p>
+      ),
+    },
+  ];
 
-  const loadingColumns = React.useMemo(
-    () => [
-      {
-        Header: "Market Name",
-        accessor: "exchangeName",
-        Cell: (tableProps) =>
-          tableProps.row.original.exchangeName ? (
+  const nonFetchedColumns = [
+    {
+      Header: "Market Name",
+      accessor: "name",
+      Cell: (tableProps) => (
+        <div className="d-flex align-items-center justify-content-around">
+          <img
+            src={`/tradePage/${tableProps?.row?.original?.logo}.webp`}
+            width={20}
+            alt="logo"
+          />
+          <a
+            href={tableProps.row.original.url}
+            target="_blank"
+            width={20}
+            rel="noreferrer"
+          >
+            {tableProps.row.original.name}
+          </a>{" "}
+          <i className="bi bi-arrow-up-right"></i>
+        </div>
+      ),
+      sortMethod: (a, b) => a - b,
+    },
+    { Header: "Trade Pair", accessor: "pair" },
+    {
+      Header: "Price",
+      accessor: "price",
+      Cell: (tableProps) => (
+        <div>
+          {isLoadingTrades ? (
             <p className="placeholder-glow">
               <span className="placeholder col-6"></span>
             </p>
-          ) : null,
-      },
-      {
-        Header: "Trade Pair",
-        accessor: "tradePair",
-        Cell: (tableProps) =>
-          tableProps.row.original.tradePair ? (
+          ) : (
+            <p>
+              {" "}
+              $
+              {getBalanceStyle(
+                decimalize(tableProps.row.original.price),
+                "caption text-white-300",
+                "caption2 text-white-300"
+              )}
+            </p>
+          )}
+        </div>
+      ),
+      sortMethod: (a, b) => parseFloat(a) - parseFloat(b),
+    },
+    {
+      Header: "Volume(24Hour)",
+      accessor: "volume",
+      Cell: (tableProps) => (
+        <div>
+          {isLoadingTrades ? (
             <p className="placeholder-glow">
               <span className="placeholder col-6"></span>
             </p>
-          ) : null,
-      },
-      {
-        Header: "Price",
-        accessor: "price",
-        Cell: (tableProps) =>
-          tableProps.row.original.price ? (
-            <p className="placeholder-glow">
-              <span className="placeholder col-6"></span>
+          ) : (
+            <p>
+              {getBalanceStyle(
+                decimalize(tableProps.row.original.volume),
+                "caption text-white-300",
+                "caption2 text-white-300"
+              )}
             </p>
-          ) : null,
-      },
-      {
-        Header: "Volume(24Hour)",
-        accessor: "volume",
-        Cell: (tableProps) =>
-          tableProps.row.original.volume ? (
-            <p className="placeholder-glow">
-              <span className="placeholder col-6"></span>
-            </p>
-          ) : null,
-      },
-    ],
-    []
-  );
-
-  const errorColumns = React.useMemo(
-    () => [
-      {
-        Header: "Market Name",
-        accessor: "name",
-        Cell: (tableProps) => (
-          <div className="d-flex align-items-center justify-content-around">
-            <img
-              src={`/tradePage/${tableProps?.row?.original?.logo}.webp`}
-              width={20}
-              alt="logo"
-            />
-            <a
-              href={tableProps.row.original.url}
-              target="_blank"
-              width={20}
-              rel="noreferrer"
-            >
-              {tableProps.row.original.name}
-            </a>{" "}
-            <i className="bi bi-arrow-up-right"></i>
-          </div>
-        ),
-        sortMethod: (a, b) => a - b,
-      },
-      { Header: "Trade Pair", accessor: "pair" },
-      {
-        Header: "Price",
-        accessor: "price",
-        Cell: (tableProps) => (
-          <p>
-            $
-            {getBalanceStyle(
-              decimalize(tableProps.row.original.price),
-              "caption text-white-300",
-              "caption2 text-white-300"
-            )}
-          </p>
-        ),
-        sortMethod: (a, b) => parseFloat(a) - parseFloat(b),
-      },
-      {
-        Header: "Volume(24Hour)",
-        accessor: "volume",
-        Cell: (tableProps) => (
-          <p>
-            {getBalanceStyle(
-              decimalize(tableProps.row.original.volume),
-              "caption text-white-300",
-              "caption2 text-white-300"
-            )}
-          </p>
-        ),
-      },
-    ],
-    []
-  );
+          )}
+        </div>
+      ),
+    },
+  ];
 
   let columns = [];
-  if (isLoadingTrades) {
-    columns = loadingColumns;
-  } else if (errorTrades) {
-    columns = errorColumns;
+  if (isLoadingTrades || errorTrades) {
+    columns = nonFetchedColumns;
   } else {
     columns = fetchedColumns;
   }
