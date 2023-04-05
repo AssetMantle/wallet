6c8750c2624d4654c8fa91e5d23e7085d43a256f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-05 15:57:08 +0600

    💅🛠️made responsive

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Balance from "../views/Balance";
 import Header from "../views/Header";
 import Banner from "./Banner";
@@ -6,21 +6,37 @@ import Banner from "./Banner";
 import ScrollableSectionContainer from "./ScrollableSectionContainer";
 
 export default function Layout({ children }) {
+  const [leftCol, setLeftCol] = useState(false);
   return (
     <>
       <div className="d-none d-md-block am_app_container">
         <Banner />
-        <Header />
+        <Header setLeftCol={setLeftCol} />
         <main
           className="container-xxl pt-4 h-100"
           style={{ maxWidth: "1920px" }}
         >
-          <div className="row px-2 position-relative h-100">
-            <ScrollableSectionContainer className="col-3 d-flex flex-column gap-3">
+          <div className="row px-2 position-relative h-100 pb-2">
+            <ScrollableSectionContainer
+              className={`col-12 col-lg-3 d-none d-lg-flex flex-column gap-3 ${
+                leftCol
+                  ? "am_resp_right_ham w-50 top-0 start-0 bottom-0 d-flex align-items-center justify-content-center nav-bg p-4 p-lg-0 gap-5 gap-lg-3"
+                  : ""
+              }`}
+            >
               <Balance />
               {/* <Vesting /> */}
+              {leftCol && (
+                <div
+                  className="d-flex d-lg-none position-absolute top-0 end-0 p-3"
+                  role="button"
+                  onClick={() => setLeftCol(false)}
+                >
+                  <i className="bi bi-x-lg h2"></i>
+                </div>
+              )}
             </ScrollableSectionContainer>
-            <div className="col-9 h-100">{children}</div>
+            <div className="col-12 col-lg-9 h-100">{children}</div>
           </div>
         </main>
       </div>
