d03e19068917d53f94757d77aab796bae7e10a90
Jathin Kotian <jathin@assetmantle.one>
2023-04-06 15:14:04 +0530

    added loading placeholders for availableBalance component

--- components/swrComponents/availableBalance.jsx
+++ components/swrComponents/availableBalance.jsx
@@ -21,22 +21,37 @@ export const AvailableBalance = () => {
   console.log("inside AvailableBalance");
   const walletManager = useChain(defaultChainName);
   const { status } = walletManager;
-  const { availableBalance } = useAvailableBalance();
+  const { availableBalance, isLoadingAvailableBalance } = useAvailableBalance();
 
-  const balanceDisplay =
-    status === "Connected"
-      ? getBalanceStyle(fromChainDenom(availableBalance), "caption", "caption2")
-      : getBalanceStyle(
-          fromChainDenom(availableBalance),
-          "caption text-gray",
-          "caption2 text-gray"
-        );
+  const isConnected = status == "Connected";
+
+  const balanceDisplay = isConnected
+    ? getBalanceStyle(fromChainDenom(availableBalance), "caption", "caption2")
+    : getBalanceStyle(
+        fromChainDenom(availableBalance),
+        "caption text-gray",
+        "caption2 text-gray"
+      );
 
   return (
     <>
-      <p className={status === "Connected" ? "caption" : "caption text-gray"}>
-        {balanceDisplay} {denomDisplay}
-      </p>
+      {isConnected ? (
+        <div className="caption">
+          {isLoadingAvailableBalance ? (
+            <p className="placeholder-glow">
+              <span className="placeholder col-6 bg-light w-100"></span>
+            </p>
+          ) : (
+            <>
+              {balanceDisplay} {denomDisplay}
+            </>
+          )}{" "}
+        </div>
+      ) : (
+        <div className="caption text-gray">
+          {balanceDisplay} {denomDisplay}
+        </div>
+      )}
     </>
   );
 };
@@ -48,7 +63,7 @@ export const AvailableBalanceUsd = () => {
   const { availableBalance, isLoadingAvailableBalance } = useAvailableBalance();
   const { mntlUsdValue } = useMntlUsd();
 
-  const isConnected = status == "Connected" && !isLoadingAvailableBalance;
+  const isConnected = status == "Connected";
 
   const balanceInUSD = BigNumber(fromDenom(availableBalance))
     .multipliedBy(BigNumber(mntlUsdValue || 0))
@@ -63,9 +78,26 @@ export const AvailableBalanceUsd = () => {
       );
 
   return (
-    <p className={isConnected ? "caption2" : "caption2 text-gray"}>
-      {balanceInUSDDisplay}
-      &nbsp;{usdSymbol}
-    </p>
+    <>
+      {" "}
+      {isConnected ? (
+        <div className="caption">
+          {isLoadingAvailableBalance ? (
+            <p className="placeholder-glow">
+              <span className="placeholder col-6 bg-light w-100"></span>
+            </p>
+          ) : (
+            <>
+              {" "}
+              {balanceInUSDDisplay} {usdSymbol}
+            </>
+          )}{" "}
+        </div>
+      ) : (
+        <div className="caption text-gray">
+          {balanceInUSDDisplay} {usdSymbol}
+        </div>
+      )}
+    </>
   );
 };
