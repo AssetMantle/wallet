3690a826479cf8a84014d65e0fe0a7601c318d57
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-04 13:12:07 +0600

    📕🛠️💅🪲bs to react-bs

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Alert } from "react-bootstrap";
 import Slider from "react-slick";
 
 export default function Banner() {
@@ -35,102 +36,67 @@ export default function Banner() {
   const settings = {
     infinite: true,
     speed: 2000,
+    autoplaySpeed: 4000,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: false,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 5000,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
+
+  const ALERTs = [
+    {
+      href: "https://marketplace.assetmantle.one/",
+      text: (
+        <>
+          🎉{"  "}Click here for limited early access to{" "}
+          <strong>MantlePlace NFTs</strong>
+          {"  "}🎉
+        </>
+      ),
+      target: "_blank",
+    },
+    {
+      href: "https://hackenproof.com/assetmantle/assetmantle",
+      text: (
+        <>
+          🏆 Participate in the Bug Bounty Program <strong>here</strong> and win
+          prizes! 🏆
+        </>
+      ),
+      target: "_blank",
+    },
+    {
+      href: "https://quickswap.exchange/#/farm/v2?tab=DualFarm",
+      text: <>💰 QuickSwap farming rewards are live now! Click Here! 💰</>,
+      target: "_blank",
+    },
+  ];
   return (
-    <Slider
-      // options={{
-      //   type: "loop",
-      //   rewind: true,
-      //   width: "100%",
-      //   gap: "32px",
-      //   perPage: 1,
-      //   pagination: false,
-      //   autoplay: true,
-      //   interval: 4000,
-      //   speed: 2000,
-      //   arrowPath:
-      //     "M23 13L21.59 14.41L26.17 19H10V21H26.17L21.58 25.59L23 27L30 20L23 13Z",
-      // }}
-      className="bg-yellow-100"
-      {...settings}
-    >
-      <div>
-        <div
-          className="alert alert-warning alert-dismissible fade show m-0 rounded-0 py-1 text-center bg-yellow-100 border-0 text-dark d-flex px-2"
-          role="alert"
-        >
-          <a
-            className="text-dark-hover mx-auto"
-            style={{ width: "80%" }}
-            href="https://marketplace.assetmantle.one/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            🎉{"  "}Click here for limited early access to{" "}
-            <strong>MantlePlace NFTs</strong>
-            {"  "}🎉
-          </a>
-          {/* <button
-    type="button"
-    className="btn-close py-2"
-    data-bs-dismiss="alert"
-    aria-label="Close"
-  ></button> */}
-        </div>
-      </div>
-      <div>
-        <div
-          className="alert alert-warning alert-dismissible fade show m-0 rounded-0 py-1 text-center bg-yellow-100 border-0 text-dark d-flex px-2"
-          role="alert"
-        >
-          <a
-            className="text-dark-hover mx-auto"
-            style={{ maxWidth: "80%" }}
-            href="https://hackenproof.com/assetmantle/assetmantle"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            🏆 Participate in the Bug Bounty Program <strong>here</strong> and
-            win prizes! 🏆
-          </a>
-          {/* <button
-        type="button"
-        className="btn-close py-2"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-      ></button> */}
-        </div>
-      </div>
-      <div>
-        <div
-          className="alert alert-warning alert-dismissible fade show m-0 rounded-0 py-1 text-center bg-yellow-100 border-0 text-dark d-flex px-2"
-          role="alert"
-        >
-          <a
-            className="text-dark-hover mx-auto"
-            style={{ maxWidth: "80%" }}
-            href="https://quickswap.exchange/#/farm/v2?tab=DualFarm"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            💰 QuickSwap farming rewards are live now! Click Here! 💰
-          </a>
-          {/* <button
-        type="button"
-        className="btn-close py-2"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-      ></button> */}
-        </div>
-      </div>
-    </Slider>
+    ALERTs &&
+    Array.isArray(ALERTs) &&
+    ALERTs.length > 0 && (
+      <Slider className="bg-primary" {...settings}>
+        {React.Children.toArray(
+          ALERTs.map((alert) => (
+            <Alert
+              className="m-0 py-1 text-center border-0 d-flex px-2"
+              variant="warning"
+            >
+              <Alert.Link
+                className="text-center px-3 text-dark"
+                style={{ width: "100%" }}
+                href={alert.href}
+                target={alert.target}
+                rel="noopener noreferrer"
+              >
+                {alert.text}
+              </Alert.Link>
+            </Alert>
+          ))
+        )}
+      </Slider>
+    )
   );
 }
