66da1a8b5d416d288de264470f6aec3d6b8a0a48
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-08 15:39:01 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,7 @@ import Header from "../views/Header";
 import Banner from "./Banner";
 // import Vesting from "../views/Vesting";
 import ScrollableSectionContainer from "./ScrollableSectionContainer";
+import { Stack } from "react-bootstrap";
 
 export default function Layout({ children }) {
   const [leftCol, setLeftCol] = useState(false);
@@ -20,7 +21,7 @@ export default function Layout({ children }) {
             <ScrollableSectionContainer
               className={`col-12 col-lg-3 d-none d-lg-flex flex-column gap-3 ${
                 leftCol
-                  ? "am_resp_right_ham w-50 top-0 start-0 bottom-0 d-flex align-items-center justify-content-center nav-bg p-4 p-lg-0 gap-5 gap-lg-3"
+                  ? "am_resp_right_ham w-50 top-0 start-0 bottom-0 d-flex align-items-center justify-content-center bg-black bg-opacity-75 p-4 p-lg-0 gap-5 gap-lg-3"
                   : ""
               }`}
             >
@@ -41,29 +42,36 @@ export default function Layout({ children }) {
         </main>
       </div>
       <div
-        className="d-flex d-md-none overflow-hidden show-on-mobile align-items-center justify-content-center p-3"
+        className="d-flex d-md-none overflow-hidden align-items-center justify-content-center p-3"
         style={{ height: "100dvh" }}
       >
-        <div
-          className="d-flex flex-column rounded-3 bg-gray-700 overflow-hidden"
+        <Stack
+          className="rounded-3 bg-body-secondary overflow-hidden flex-grow-0 h-auto m-auto"
           style={{ width: "min(400px,100%)" }}
         >
-          <div className="d-flex align-items-center justify-content-between nav-bg p-3">
-            <div className="d-flex gap-2">
+          <Stack
+            direction="horizontal"
+            className="align-items-center justify-content-between bg-black p-3"
+          >
+            <Stack direction="horizontal" gap={2}>
               <i className="bi bi-circle-fill text-error"></i>
               <i className="bi bi-circle-fill text-primary"></i>
               <i className="bi bi-circle-fill text-success"></i>
-            </div>
-            <i className="bi bi-info-circle text-blue-300"></i>
-          </div>
-          <div className="h2 d-flex align-items-center gap-4 p-3">
+            </Stack>
+            <i className="bi bi-info-circle text-info"></i>
+          </Stack>
+          <Stack
+            className="h2 align-items-center p-3"
+            direction="horizontal"
+            gap={4}
+          >
             <i className="bi bi-exclamation-triangle-fill"></i>
             <h1 className="m-auto text-primary text-center">
               Please open the app on a Tab or Larger device.
             </h1>
             <i className="bi bi-exclamation-triangle-fill"></i>
-          </div>
-        </div>
+          </Stack>
+        </Stack>
       </div>
     </>
   );
