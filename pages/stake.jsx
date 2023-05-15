fdd9df41c81fa3dca98520ee59f7552eb768f462
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-15 14:46:14 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- pages/stake.jsx
+++ pages/stake.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AllValidators from "../components/AllValidators";
 import DelegatedValidators from "../components/DelegatedValidators";
-import Tooltip from "../components/Tooltip";
 import { useAllValidators, useDelegatedValidators } from "../data";
 import useStakeReducer from "../data/useStakeReducer";
 import StakedToken from "../views/StakedToken";
@@ -10,6 +9,16 @@ import Head from "next/head";
 import { toast } from "react-toastify";
 import Link from "next/link";
 import { toastConfig } from "../config";
+import {
+  Button,
+  Col,
+  OverlayTrigger,
+  Row,
+  Stack,
+  // ToggleButton,
+  // ToggleButtonGroup,
+  Tooltip,
+} from "react-bootstrap";
 
 export default function Stake() {
   const [searchValue, setSearchValue] = useState("");
@@ -60,125 +69,142 @@ export default function Stake() {
   };
 
   // transaction manifest modal states and functions
-  const GasOptions = [
-    {
-      name: "Zero",
-      usd: "0.0000",
-      mntl: "0.0000",
-    },
-    {
-      name: "Low",
-      usd: "0.0000",
-      mntl: "0.0000",
-    },
-    {
-      name: "High",
-      usd: "0.0000",
-      mntl: "0.0000",
-    },
-  ];
-  const [SelectedGasFee, setSelectedGasFee] = useState(GasOptions[0].name);
-  const [ManifestShowAdvanced, setManifestShowAdvanced] = useState(false);
-  const [ManifestKeystorePassword, setManifestKeystorePassword] = useState();
-  const [ManifestCustomGas, setManifestCustomGas] = useState();
-  const handleManifestConfirm = () => {
-    console.log("Confirming transaction manifest");
-  };
+  // const GasOptions = [
+  //   {
+  //     name: "Zero",
+  //     usd: "0.0000",
+  //     mntl: "0.0000",
+  //   },
+  //   {
+  //     name: "Low",
+  //     usd: "0.0000",
+  //     mntl: "0.0000",
+  //   },
+  //   {
+  //     name: "High",
+  //     usd: "0.0000",
+  //     mntl: "0.0000",
+  //   },
+  // ];
+  // const [SelectedGasFee, setSelectedGasFee] = useState(GasOptions[0].name);
+  // const [ManifestShowAdvanced, setManifestShowAdvanced] = useState(false);
+  // const [ManifestKeystorePassword, setManifestKeystorePassword] = useState();
+  // const [ManifestCustomGas, setManifestCustomGas] = useState();
+  // const handleManifestConfirm = () => {
+  //   console.log("Confirming transaction manifest");
+  // };
 
   return (
     <>
       <Head>
         <title>Stake | MantleWallet</title>
       </Head>
-      <section className="row h-100">
-        <div className="col-8 h-100">
-          <div
-            className="bg-gray-800 p-3 rounded-4 d-flex flex-column gap-2"
+      <Row className="row h-100" as="section">
+        <Col xs={8} className="h-100 pb-2">
+          <Stack
+            gap={2}
+            className="bg-light-subtle p-3 rounded-4"
             style={{ height: "90%" }}
           >
-            <div className="d-flex align-items-center justify-content-between w-100">
-              <div className="card-title body1 text-primary my-auto">
+            <Stack
+              direction="horizontal"
+              className="align-items-center justify-content-between w-100"
+            >
+              <h1 className="card-title body1 text-primary m-0 my-auto">
                 Validators
-              </div>
+              </h1>
               <div className="btn-group">
-                <button
-                  className={`${
-                    activeValidators ? "btn btn-primary" : "btn btn-inactive"
-                  } caption`}
+                <Button
+                  variant={activeValidators ? "primary" : "outline-light"}
                   onClick={() => {
                     setActiveValidators(true);
                     stakeDispatch({ type: "EMPTY_SELECTED_VALIDATORS" });
                   }}
                 >
                   Active
-                </button>
-                <button
-                  className={`${
-                    !activeValidators ? "btn btn-primary" : "btn btn-inactive"
-                  } caption`}
+                </Button>
+                <Button
+                  variant={!activeValidators ? "primary" : "outline-light"}
                   onClick={() => {
                     setActiveValidators(false);
                     stakeDispatch({ type: "EMPTY_SELECTED_VALIDATORS" });
                   }}
                 >
                   Inactive
-                </button>
+                </Button>
               </div>
-            </div>
+            </Stack>
             <div
-              className="d-flex flex-column w-100 nav-bg p-2 rounded-4 flex-grow-1"
+              className="d-flex flex-column w-100 bg-black p-2 rounded-4 flex-grow-1"
               style={{ height: "88%" }}
             >
-              <div className="d-flex align-items-center gap-3 w-100">
-                <div
-                  className="d-flex gap-2 am-input border-color-white rounded-3 py-1 px-3 align-items-center"
-                  style={{ flex: "1" }}
+              <Stack
+                direction="horizontal"
+                gap={3}
+                className="align-items-center w-100"
+              >
+                <Stack
+                  gap={2}
+                  direction="horizontal"
+                  className="border border-white rounded-3 py-1 px-3 align-items-center flex-grow-1"
                 >
-                  <span
-                    className="input-group-text bg-t p-0 h-100"
+                  {/* <span
+                    className="input-group-text bg-transparent p-0 h-100"
                     id="basic-addon1"
                     style={{ border: "none" }}
                   >
-                    <i className="bi bi-search text-white"></i>
-                  </span>
+                  </span> */}
+                  <i className="bi bi-search text-white"></i>
                   <input
                     type="search"
-                    className="am-input bg-t p-1 w-100 h-100"
+                    className="bg-transparent border border-0 p-1 w-100 h-100"
                     placeholder="Search"
                     aria-label="Search"
                     value={searchValue}
                     onChange={(e) => setSearchValue(e.target.value)}
-                    style={{ border: "none" }}
                   />
-                </div>
-                <div
-                  className="d-flex gap-2 align-items-center text-white"
+                </Stack>
+                <Stack
+                  as="button"
+                  direction="horizontal"
+                  gap={2}
+                  className="align-items-center text-white"
+                  role="button"
                   onClick={() => {
                     setDelegated((prev) => !prev);
                     stakeDispatch({ type: "EMPTY_SELECTED_VALIDATORS" });
                   }}
                 >
                   Delegated
-                  <Tooltip description="Showcase a list of all validators that you have ever delegated tokens with." />
-                  <button
+                  <OverlayTrigger
+                    overlay={
+                      <Tooltip id={"delegated-stake"}>
+                        Showcase a list of all validators that you have ever
+                        delegated tokens with.
+                      </Tooltip>
+                    }
+                  >
+                    <i className="bi bi-info-circle"></i>
+                  </OverlayTrigger>
+                  <div
                     className={`d-flex rounded-4 align-items-center transitionAll ${
                       delegated
-                        ? "bg-yellow-100 justify-content-end"
-                        : "bg-theme-white justify-content-start"
+                        ? "bg-primary justify-content-end"
+                        : "bg-white justify-content-start"
                     }`}
                     style={{ width: "40px", padding: "2px" }}
                   >
-                    <div className="p-2 rounded-4 bg-dark-200"></div>
-                  </button>
-                </div>
-              </div>
+                    <span className="p-2 rounded-4 bg-dark"></span>
+                  </div>
+                </Stack>
+              </Stack>
               <div className="w-100 mt-3 h-100" style={{ overflow: "auto" }}>
                 <table
                   className="table"
                   style={{ width: "max-content", minWidth: "100%" }}
                 >
                   <thead
-                    className="position-sticky top-0 nav-bg"
+                    className="position-sticky top-0 bg-black"
                     style={{
                       zIndex: "200",
                     }}
@@ -262,23 +288,25 @@ export default function Stake() {
                 </table>
               </div>
             </div>
-          </div>
-        </div>
+          </Stack>
+        </Col>
 
-        <ScrollableSectionContainer className="col-4">
-          <StakedToken
-            delegated={delegated}
-            setDelegated={setDelegated}
-            notify={notify}
-            stakeState={stakeState}
-            stakeDispatch={stakeDispatch}
-            showClaimError={showClaimError}
-            setShowClaimError={setShowClaimError}
-            totalTokens={totalTokens}
-            selectedValidators={stakeState?.selectedValidators}
-          />
-        </ScrollableSectionContainer>
-      </section>
+        <Col xs={4} className="h-100 pb-2">
+          <ScrollableSectionContainer>
+            <StakedToken
+              delegated={delegated}
+              setDelegated={setDelegated}
+              notify={notify}
+              stakeState={stakeState}
+              stakeDispatch={stakeDispatch}
+              showClaimError={showClaimError}
+              setShowClaimError={setShowClaimError}
+              totalTokens={totalTokens}
+              selectedValidators={stakeState?.selectedValidators}
+            />
+          </ScrollableSectionContainer>
+        </Col>
+      </Row>
     </>
   );
 }
