1f807d81bc7bcf7ae9c270b3bbefac847782c140
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-18 13:26:44 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- views/VoteInfo.js
+++ views/VoteInfo.js
@@ -1,3 +1,4 @@
+import { Stack } from "react-bootstrap";
 import { useDelegatedValidators } from "../data";
 import { useAllValidators } from "../data";
 
@@ -19,28 +20,38 @@ const VoteInfo = ({ isConnected }) => {
 
   return (
     // <div className="col-12 pt-3 pt-lg-0 col-lg-4">
-    <div className="rounded-4 p-3 bg-gray-800 width-100 d-flex flex-column gap-2 transitionAll">
-      <nav className="d-flex align-items-center justify-content-between gap-3">
-        <div className="d-flex gap-3 align-items-center">
+    <Stack
+      className="rounded-4 p-3 bg-light-subtle width-100 transitionAll flex-grow-0"
+      gap={2}
+    >
+      <Stack
+        as="nav"
+        direction="horizontal"
+        gap={3}
+        className="align-items-center justify-content-between"
+      >
+        <Stack direction="horizontal" gap={3} className="align-items-center">
           <button className={`body2 text-primary`}>Voting Statistics</button>
-        </div>
-      </nav>
-      <div className="nav-bg rounded-4 d-flex flex-column p-3 gap-2 align-items-start">
-        <p className={isConnected ? "caption" : "caption text-gray"}>
+        </Stack>
+      </Stack>
+      <Stack gap={2} className="bg-black rounded-4 p-3 align-items-start">
+        <p className={`caption m-0 ${isConnected ? "" : "text-body"}`}>
           Your Voting Power is{" "}
         </p>
         {isConnected ? (
           totalDelegatedAmount && allValidators ? (
-            <p>{((totalDelegatedAmount * 100) / totalTokens).toFixed(10)}%</p>
+            <p className="m-0">
+              {((totalDelegatedAmount * 100) / totalTokens).toFixed(10)}%
+            </p>
           ) : (
-            <p>0%</p>
+            <p className="m-0">0%</p>
           )
         ) : (
-          <p className="text-gray">0%</p>
+          <p className="text-body m-0">0%</p>
         )}
         <br />
-      </div>
-    </div>
+      </Stack>
+    </Stack>
     // </div>
   );
 };
