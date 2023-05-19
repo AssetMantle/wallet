c6b0ff2172a2d303a868852fe26597f4c23b9bd9
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-19 12:43:32 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- pages/bridge.jsx
+++ pages/bridge.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import ICTransactionInfo from "../components/ICTransactionInfo";
 import ScrollableSectionContainer from "../components/ScrollableSectionContainer";
 import Tooltip from "../components/Tooltip";
@@ -6,52 +7,69 @@ import EthToPolygonBridge from "../views/EthToPolygonBridge";
 import GravityToEthBridge from "../views/GravityToEthBridge";
 import MntlToGravityBridge from "../views/MntlToGravityBridge";
 import PolygonBridge from "../views/PolygonBridge";
+import { Col, Row, Stack } from "react-bootstrap";
 
 export default function Bridge() {
   return (
     <>
-      <main className="row h-100">
-        <ScrollableSectionContainer className="col-8">
-          <section className="rounded-4 p-3 bg-gray-800 width-100 d-flex flex-column gap-3 transitionAll">
-            <nav className="d-flex align-items-center justify-content-between gap-3">
-              <div className="d-flex gap-3 align-items-center">
-                <h1 className="body1 text-primary">Interchain</h1>
-              </div>
-            </nav>
-            <div className="nav-bg d-flex flex-column gap-3 rounded-4 p-3">
-              <MntlToGravityBridge />
-              <GravityToEthBridge />
-              <EthToPolygonBridge />
-              <PolygonBridge />
+      <Head>
+        <title>Bridge | MantleWallet</title>
+      </Head>
+      <Row as={`main`} className="h-100">
+        <Col xs={8}>
+          <ScrollableSectionContainer className="">
+            <Stack
+              as={`section`}
+              className="rounded-4 p-3 bg-light-subtle width-100 flex-grow-0"
+              gap={3}
+            >
+              <Stack
+                as="nav"
+                direction="horizontal"
+                gap={3}
+                className="align-items-center justify-content-between"
+              >
+                <Stack gap={3} className="">
+                  <h1 className="body1 text-primary">Interchain</h1>
+                </Stack>
+              </Stack>
+              <Stack gap={3} className="bg-black rounded-4 p-3">
+                <MntlToGravityBridge />
+                <GravityToEthBridge />
+                <EthToPolygonBridge />
+                <PolygonBridge />
+              </Stack>
+            </Stack>
+          </ScrollableSectionContainer>
+        </Col>
+        <Col xs={4}>
+          <ScrollableSectionContainer className="d-flex flex-column gap-3">
+            <div className="rounded-4 p-3 bg-light-subtle width-100 text-white-300">
+              <Tooltip
+                titlePrimary={true}
+                description={""}
+                style={{ right: "330%" }}
+              />
+              &nbsp;The Order in which you need to complete the transactions:
             </div>
-          </section>
-        </ScrollableSectionContainer>
-        <ScrollableSectionContainer className="col-4 d-flex flex-column gap-3">
-          <div className="rounded-4 p-3 bg-gray-800 width-100 text-white-300">
-            <Tooltip
-              titlePrimary={true}
-              description={""}
-              style={{ right: "330%" }}
+            <ICTransactionInfo
+              title="1. Send to Gravity (5s)"
+              chainFrom="assetmantle"
+              chainTo="gravitybridge"
             />
-            &nbsp;The Order in which you need to complete the transactions:
-          </div>
-          <ICTransactionInfo
-            title="1. Send to Gravity (5s)"
-            chainFrom="assetmantle"
-            chainTo="gravitybridge"
-          />
-          <ICTransactionInfo
-            title="2. Send to Ethereum (30m)"
-            chainFrom="gravitybridge"
-            chainTo="ethereum"
-          />
-          <ICTransactionInfo
-            title="3. Send to Polygon (30m)"
-            chainFrom="ethereum"
-            chainTo="polygon"
-          />
-        </ScrollableSectionContainer>
-      </main>
+            <ICTransactionInfo
+              title="2. Send to Ethereum (30m)"
+              chainFrom="gravitybridge"
+              chainTo="ethereum"
+            />
+            <ICTransactionInfo
+              title="3. Send to Polygon (30m)"
+              chainFrom="ethereum"
+              chainTo="polygon"
+            />
+          </ScrollableSectionContainer>
+        </Col>
+      </Row>
     </>
   );
 }
