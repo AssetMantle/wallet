5c531b1950a6963a7ecc5cb241e2e6039e8ddd8d
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-19 12:24:06 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- views/Portfolio.js
+++ views/Portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Stack } from "react-bootstrap";
 
 export default function Portfolio() {
   const dataSet = {
@@ -7,36 +8,44 @@ export default function Portfolio() {
     usd: "0.83",
   };
   return (
-    <section className="rounded-5 p-4 bg-gray-800 width-100 d-flex flex-column gap-3">
+    <Stack
+      gap={3}
+      as={"section"}
+      className="rounded-5 p-4 bg-light-subtle width-100 flex-grow-0"
+    >
       <h4 className="body1 text-primary">Portfolio</h4>
-      <div className="nav-bg p-3 rounded-4 gap-3">
-        <div className="d-flex flex-column gap-2">
-          <p className="caption d-flex gap-2 align-items-center">
-            Vesting{" "}
-            <span>
-              <i className="bi bi-info-circle" />
-            </span>
-          </p>
-          <p className="caption">{dataSet.vesting} $MNTL</p>
-          <p className="caption">
+      <div className="bg-black p-3 rounded-4 gap-3">
+        <Stack gap={2}>
+          <Stack
+            as={`p`}
+            gap={2}
+            direction="horizontal"
+            className="caption align-items-center m-0"
+          >
+            Vesting <i className="bi bi-info-circle" />
+          </Stack>
+          <p className="caption m-0">{dataSet.vesting} $MNTL</p>
+          <p className="caption m-0">
             {Number(dataSet.vesting) * Number(dataSet.usd)} USD
           </p>
-        </div>
+        </Stack>
       </div>
-      <div className="nav-bg p-3 rounded-4 gap-3">
-        <div className="d-flex flex-column gap-2">
-          <p className="caption d-flex gap-2 align-items-center">
-            Delegatable{" "}
-            <span>
-              <i className="bi bi-info-circle" />
-            </span>
-          </p>
-          <p className="caption">{dataSet.delegable} $MNTL</p>
-          <p className="caption">
+      <div className="bg-black p-3 rounded-4 gap-3">
+        <Stack gap={2}>
+          <Stack
+            as={`p`}
+            gap={2}
+            direction="horizontal"
+            className="caption align-items-center m-0"
+          >
+            Delegatable <i className="bi bi-info-circle" />
+          </Stack>
+          <p className="caption m-0">{dataSet.delegable} $MNTL</p>
+          <p className="caption m-0">
             {Number(dataSet.delegable) * Number(dataSet.usd)} USD
           </p>
-        </div>
+        </Stack>
       </div>
-    </section>
+    </Stack>
   );
 }
