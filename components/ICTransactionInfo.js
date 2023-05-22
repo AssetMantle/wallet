6b650f4d108e4429e35e8f17ff9df7d1f803e593
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-22 19:41:18 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- components/ICTransactionInfo.js
+++ components/ICTransactionInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Stack } from "react-bootstrap";
 
 export default function ICTransactionInfo({ title, chainFrom, chainTo }) {
   const ChainImage = (name) => {
@@ -23,35 +24,47 @@ export default function ICTransactionInfo({ title, chainFrom, chainTo }) {
     return res;
   };
   return (
-    <section className="rounded-4 p-3 bg-gray-800 width-100 d-flex flex-column gap-3">
-      <h4 className="h3 text-primary">{title}</h4>
-      <div className="nav-bg p-2 rounded-4 d-flex flex-column align-items-center gap-2">
-        <div className="d-flex align-items-center justify-content-between gap-2 w-100">
-          <div className="d-flex gap-1 align-items-center">
+    <Stack
+      as="section"
+      gap={3}
+      className="rounded-4 p-3 bg-light-subtle width-100"
+    >
+      <h4 className="h3 text-primary m-0">{title}</h4>
+      <Stack gap={2} className="bg-black p-2 rounded-4 align-items-center">
+        <Stack
+          direction="horizontal"
+          gap={2}
+          className="align-items-center justify-content-between w-100"
+        >
+          <Stack direction="horizontal" gap={1} className="align-items-center">
             <div
               className="position-relative"
               style={{ width: "20px", aspectRatio: "1/1" }}
             >
               <img layout="fill" src={ChainImage(chainFrom)} alt={chainFrom} />
             </div>
-            <p className="caption2 text-primary">{chainFrom}</p>
-          </div>
-        </div>
+            <p className="caption2 text-primary m-0">{chainFrom}</p>
+          </Stack>
+        </Stack>
         <div className="body2 mx-auto text-primary">
           <i className="bi bi-arrow-down" />
         </div>
-        <div className="d-flex align-items-center justify-content-between gap-2 w-100">
-          <div className="d-flex gap-1 align-items-center">
+        <Stack
+          direction="horizontal"
+          gap={2}
+          className="align-items-center justify-content-between w-100"
+        >
+          <Stack direction="horizontal" gap={1} className="align-items-center">
             <div
               className="position-relative"
               style={{ width: "20px", aspectRatio: "1/1" }}
             >
               <img layout="fill" src={ChainImage(chainTo)} alt={chainTo} />
             </div>
-            <p className="caption2 text-primary">{chainTo}</p>
-          </div>
-        </div>
-      </div>
-    </section>
+            <p className="caption2 text-primary m-0">{chainTo}</p>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
