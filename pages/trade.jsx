ab6c1bc42ed1a8034e6cedfa1587b7c167a04aa0
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-24 14:23:00 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- pages/trade.jsx
+++ pages/trade.jsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import TradeTable from "../components/TradeTable";
 import TradePageTokenDetails from "../components/TradePageTokenDetails";
 import ScrollableSectionContainer from "../components/ScrollableSectionContainer";
+import { Col, Row, Stack } from "react-bootstrap";
 
 export default function Trade() {
   const [searchValue, setSearchValue] = useState("");
@@ -12,40 +13,47 @@ export default function Trade() {
       <Head>
         <title>Trade | MantleWallet</title>
       </Head>
-      <section className="row h-100">
-        <ScrollableSectionContainer className="col-8 h-100">
-          <div
-            className="bg-gray-800 p-3 pb-5 rounded-4 d-flex flex-column gap-2"
-            style={{ height: "90%" }}
-          >
-            <div className="d-flex align-items-center justify-content-between w-100">
-              <h1 className="card-title body1 text-primary my-auto">Markets</h1>
-            </div>
-            <div
-              className="d-flex flex-column w-100 nav-bg p-2 rounded-4 flex-grow-1"
+      <Row as="section" className="h-100">
+        <Col xs={8} className="h-100">
+          <ScrollableSectionContainer className="h-100">
+            <Stack
+              gap={2}
+              className="bg-light-subtle p-3 pb-5 rounded-4 flex-grow-0"
               style={{ height: "90%" }}
             >
-              <div className="w-100 h-100" style={{ overflow: "auto" }}>
-                <TradeTable />
-              </div>
-            </div>
-          </div>
-        </ScrollableSectionContainer>
-        <ScrollableSectionContainer className="col-4">
-          <div className="rounded-4 p-3 my-2 bg-gray-800 width-100 d-flex flex-column text-white">
-            <p>
-              To purchase MNTL, visit the exchanges (CEX & DEX) shown to swap
-              with your available tokens.
-            </p>
-            <br></br>
-            <p>
-              Options to directly on-ramp to MNTL using fiat currencies will be
-              coming soon.
-            </p>
-          </div>
-          <TradePageTokenDetails />
-        </ScrollableSectionContainer>
-      </section>
+              <Stack className="justify-content-between w-100">
+                <h1 className="card-title body1 text-primary my-auto text-start">
+                  Markets
+                </h1>
+              </Stack>
+              <Stack
+                className="w-100 bg-black p-2 rounded-4 flex-grow-1"
+                style={{ height: "90%" }}
+              >
+                <div className="w-100 h-100" style={{ overflow: "auto" }}>
+                  <TradeTable />
+                </div>
+              </Stack>
+            </Stack>
+          </ScrollableSectionContainer>
+        </Col>
+        <Col xs={4} className="h-100">
+          <ScrollableSectionContainer className="h-100">
+            <Stack className="rounded-4 p-3 my-2 bg-light-subtle width-100 text-white">
+              <p className="m-0">
+                To purchase MNTL, visit the exchanges (CEX & DEX) shown to swap
+                with your available tokens.
+              </p>
+              <br />
+              <p className="m-0">
+                Options to directly on-ramp to MNTL using fiat currencies will
+                be coming soon.
+              </p>
+            </Stack>
+            <TradePageTokenDetails />
+          </ScrollableSectionContainer>
+        </Col>
+      </Row>
     </>
   );
 }
