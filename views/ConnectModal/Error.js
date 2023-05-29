1a5e1b42ef487c8675c6b1340bc4195b0d0a4621
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-29 14:16:33 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- views/ConnectModal/Error.js
+++ views/ConnectModal/Error.js
@@ -1,33 +1,40 @@
 import React from "react";
+import { Button, Stack } from "react-bootstrap";
 
 export default function Error({ close, setStep }) {
   return (
-    <div className="bg-gray-800 p-4 rounded-4 w-100 my-auto">
-      <div className="d-flex align-items-center justify-content-between ">
-        <h1 className="body1 text-primary d-flex align-items-center gap-2"></h1>
-        <button className="btn text-primary body1" onClick={() => close()}>
+    <div className="bg-light-subtle p-4 rounded-4 w-100 my-auto">
+      <Stack
+        className="align-items-center justify-content-between"
+        direction="horizontal"
+      >
+        <h1 className="body1 text-primary d-flex align-items-center gap-2 m-0"></h1>
+        <Button variant="primary" className="body1" onClick={() => close()}>
           <span className="text-primary">
             <i className="bi bi-x-lg" />
           </span>
-        </button>
-      </div>
-      <div className="d-flex flex-column align-items-center justify-content-center text-center gap-3">
+        </Button>
+      </Stack>
+      <Stack
+        className="align-items-center justify-content-center text-center"
+        gap={3}
+      >
         <span
           className="text-error"
           style={{ fontSize: "calc(10px + 10vmin)" }}
         >
           <i className="bi bi-x-circle" />
         </span>
-        <h1 className="body2 text-error">Error in creating Wallet</h1>
-        <p className="caption">
+        <h1 className="body2 text-error m-0">Error in creating Wallet</h1>
+        <p className="caption m-0">
           Your wallet could not be created. Please try again.
         </p>
-        <div className="d-flex w-100 mt-2">
+        <Stack className="w-100 m-0 mt-2">
           <button className="am-link ms-auto px-3" onClick={() => setStep(4)}>
             Try again
           </button>
-        </div>
-      </div>
+        </Stack>
+      </Stack>
     </div>
   );
 }
