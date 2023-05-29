27410b748446f4a6a757faf41fe82d2c9085bf28
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-29 17:25:45 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- views/ConnectModal/MnemonicPassword.js
+++ views/ConnectModal/MnemonicPassword.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Stack } from "react-bootstrap";
 
 export default function MnemonicPassword({
   Password,
@@ -15,9 +16,9 @@ export default function MnemonicPassword({
   };
 
   return (
-    <div className="bg-gray-800 p-4 rounded-4 w-100 my-auto">
-      <div className="d-flex align-items-center justify-content-between ">
-        <h1 className="body1 text-primary d-flex align-items-center gap-2">
+    <div className="bg-light-subtle p-4 rounded-4 w-100 my-auto">
+      <Stack className="d-flex align-items-center justify-content-between ">
+        <h1 className="body1 text-primary d-flex align-items-center gap-2 m-0">
           Enter Password
         </h1>
         <button className="btn text-primary body1" onClick={() => close()}>
@@ -25,25 +26,30 @@ export default function MnemonicPassword({
             <i className="bi bi-x-lg" />
           </span>
         </button>
-      </div>
-      <p className="text-white-200 caption my-1">
+      </Stack>
+      <p className="text-white-200 caption m-0 my-1">
         Enter your password to complete creating a wallet.
       </p>
       <input
         type="password"
-        className="am-input w-100 my-3 py-2 px-3 rounded-1 bg-t border-color-white"
+        className="w-100 my-3 py-2 px-3 rounded-1 bg-transparent border border-white"
         placeholder="******"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <div className="d-flex align-items-center justify-content-end gap-3 flex-wrap mt-3">
-        <button
-          className="button-primary caption py-2 px-5"
+      <Stack
+        className="align-items-center justify-content-end flex-wrap mt-3"
+        gap={3}
+        direction="horizontal"
+      >
+        <Button
+          variant="primary"
+          className="rounded-5 caption py-2 px-5"
           onClick={() => handleConfirm()}
           disabled={Password && Password.length > 8 ? false : true}
         >
           Confirm
-        </button>
-      </div>
+        </Button>
+      </Stack>
     </div>
   );
 }
