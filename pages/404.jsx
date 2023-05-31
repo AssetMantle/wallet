f18a6d944fa59afee79bcb7fa1d1ed7be408b404
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-05-31 11:02:41 +0600

    📕🛠️💅🪲bs to react-bs and custom scss reduction

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,22 +1,25 @@
 import { Link } from "@chakra-ui/react";
 import React from "react";
 import ScrollableSectionContainer from "../components/ScrollableSectionContainer";
+import { Stack } from "react-bootstrap";
 
 export default function Custom404() {
   return (
-    <ScrollableSectionContainer className="d-flex flex-column align-items-center justify-content-center gap-3">
-      <img
-        src="/404.png"
-        style={{ width: "min(480px,100%)" }}
-        alt="404 illustration"
-      />
-      <p>
-        Page or Route not found. Go to{" "}
-        <Link className="text-primary" href={"/"}>
-          Home.
-          {/* </a> */}
-        </Link>
-      </p>
+    <ScrollableSectionContainer>
+      <Stack className="align-items-center justify-content-center" gap={3}>
+        <img
+          src="/404.png"
+          style={{ width: "min(480px,100%)" }}
+          alt="404 illustration"
+        />
+        <p>
+          Page or Route not found. Go to{" "}
+          <Link className="text-primary" href={"/"}>
+            Home.
+            {/* </a> */}
+          </Link>
+        </p>
+      </Stack>
     </ScrollableSectionContainer>
   );
 }
