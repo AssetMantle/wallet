ce71886650c03d72ba8ca54940d95e09f1912991
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-08-18 17:55:44 +0600

    🪲fix

--- components/index.jsx
+++ components/index.jsx
@@ -1,3 +1,3 @@
-export * from "../react";
-export * from "../swrComponents";
-export * from "../ConnectButton";
+export * from "./react";
+export * from "./swrComponents";
+export * from "./ConnectButton";
