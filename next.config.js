c51751765a35d2bab52c876a42fc7d702f66a10c
AssetMantle DevOps <devops@assetmantle.one>
2024-05-15 10:40:02 +0530

    updated the external urls to api routes

--- next.config.js
+++ next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
         source: "/api/osmosis/:path*",
         destination: "https://api.osmosis.zone/:path*",
       },
+      {
+        source: "/api/assetmantle/cosmosdirectoryrpc",
+        destination: "https://rpc.cosmos.directory/assetmantle",
+      },
+      {
+        source: "/api/coingecko/:path*",
+        destination: "https://api.coingecko.com/:path*",
+      },
     ];
   },
   // swcMinify: true,
