b5999d1409bcf96ef264047372f5857f52b81b06
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-09-13 14:46:12 +0600

    🛠️amount sorting added

--- components/DelegatedValidators.jsx
+++ components/DelegatedValidators.jsx
@@ -15,6 +15,7 @@ const DelegatedValidators = ({
   stakeDispatch,
   setShowClaimError,
   delegatedValidators,
+  sortParam,
 }) => {
   const {
     allValidatorsBonded,
@@ -32,97 +33,142 @@ const DelegatedValidators = ({
     e.target.src = "/validatorAvatars/alt.png";
   };
 
+  const allDelegatedValidatorFiltered = delegatedValidators
+    ?.sort((a, b) => b.tokens - a.tokens)
+    ?.filter(
+      (item) =>
+        allValidatorsBonded?.find(
+          (e) => item?.operatorAddress == e?.operatorAddress
+        ) &&
+        item?.description?.moniker
+          ?.toLowerCase()
+          ?.includes(searchValue.toLowerCase())
+    );
+
+  const allDelegatedValidatorUsable = allDelegatedValidatorFiltered?.map(
+    (el) => ({
+      ...el,
+      am_delegatedAmount: delegatedValidators?.find(
+        (element) => element?.operatorAddress == el?.operatorAddress
+      )?.delegatedAmount,
+    })
+  );
+
+  const allDelegatedValidatorsUsableSorted = (sortBy) => {
+    let ascending = true;
+    let sortStr = `${sortBy}`;
+    if (sortBy[0] === "-") {
+      ascending = false;
+      let temp = sortBy.split("");
+      sortStr = "";
+      for (let i = 1; i < temp.length; i++) sortStr += temp[i];
+    }
+
+    switch (sortStr) {
+      case "name":
+        return allDelegatedValidatorUsable?.sort((a, b) => {
+          let av = a?.description?.moniker.trim();
+          let bv = b?.description?.moniker.trim();
+          return ascending ? av?.localeCompare(bv) : bv?.localeCompare(av);
+        });
+      case "commission":
+        return allDelegatedValidatorUsable?.sort((a, b) => {
+          let av =
+            !isNaN(a?.commission?.commissionRates?.rate) &&
+            Number(a?.commission?.commissionRates?.rate);
+          let bv =
+            !isNaN(b?.commission?.commissionRates?.rate) &&
+            Number(b?.commission?.commissionRates?.rate);
+          return ascending ? av - bv : bv - av;
+        });
+      case "delegatedAmount":
+        return allDelegatedValidatorUsable?.sort((a, b) => {
+          let av =
+            !isNaN(a?.am_delegatedAmount) && Number(a?.am_delegatedAmount);
+          let bv =
+            !isNaN(b?.am_delegatedAmount) && Number(b?.am_delegatedAmount);
+          return ascending ? av - bv : bv - av;
+        });
+      case "tokens":
+      default:
+        return allDelegatedValidatorUsable?.sort((a, b) =>
+          ascending ? b?.tokens - a?.tokens : a?.tokens - b?.tokens
+        );
+    }
+  };
+
   // const statusArray = [0, 1, 2, -1];
 
   return (
     <>
       {activeValidators
-        ? delegatedValidators
-            ?.sort((a, b) => b.tokens - a.tokens)
-            ?.filter(
-              (item) =>
-                allValidatorsBonded?.find(
-                  (e) => item?.operatorAddress == e?.operatorAddress
-                ) &&
-                item?.description?.moniker
-                  ?.toLowerCase()
-                  ?.includes(searchValue.toLowerCase())
-            )
-            ?.map((item, index) => (
-              <tr key={index} className="caption2 text-white-300">
-                <td>
-                  <input
-                    type="checkbox"
-                    checked={stakeState?.selectedValidators?.includes(
+        ? allDelegatedValidatorsUsableSorted(sortParam)?.map((item, index) => (
+            <tr key={index} className="caption2 text-white-300">
+              <td>
+                <input
+                  type="checkbox"
+                  checked={stakeState?.selectedValidators?.includes(
+                    item?.operatorAddress
+                  )}
+                  onChange={() => {
+                    setShowClaimError(false);
+                    stakeState?.selectedValidators?.includes(
                       item?.operatorAddress
-                    )}
-                    onChange={() => {
-                      setShowClaimError(false);
-                      stakeState?.selectedValidators?.includes(
-                        item?.operatorAddress
-                      )
-                        ? stakeDispatch({
-                            type: "REMOVE_FROM_SELECTED_VALIDATORS",
-                            payload: item?.operatorAddress,
-                          })
-                        : stakeDispatch({
-                            type: "SET_SELECTED_VALIDATORS",
-                            payload: item?.operatorAddress,
-                          });
-                    }}
-                  ></input>
-                </td>
-                {activeValidators ? <td>{item?.rank}</td> : null}
-                <td>
-                  <div
-                    className="d-flex position-relative rounded-circle gap-1"
-                    style={{ width: "25px", aspectRatio: "1/1" }}
-                  >
-                    <img
-                      alt={item?.description?.moniker}
-                      className="rounded-circle"
-                      layout="fill"
-                      src={`/validatorAvatars/${item?.operatorAddress}.png`}
-                      onError={handleOnError}
-                    />
-                  </div>
-                </td>
-                <td className=" d-flex align-items-center justify-content-start">
-                  <a
-                    className="text-truncate"
-                    href={`https://explorer.assetmantle.one/validators/${item.operatorAddress}`}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {" "}
-                    {item?.description?.moniker}{" "}
-                    <i className="bi bi-arrow-up-right" />
-                  </a>
-                </td>
-                <td>{((item?.tokens * 100) / totalTokens).toFixed(2)}%</td>
-
-                <td>
-                  {shiftDecimalPlaces(
-                    item?.commission?.commissionRates?.rate,
-                    2
-                  )}{" "}
-                  %
-                </td>
-                <td>
+                    )
+                      ? stakeDispatch({
+                          type: "REMOVE_FROM_SELECTED_VALIDATORS",
+                          payload: item?.operatorAddress,
+                        })
+                      : stakeDispatch({
+                          type: "SET_SELECTED_VALIDATORS",
+                          payload: item?.operatorAddress,
+                        });
+                  }}
+                ></input>
+              </td>
+              {activeValidators ? <td>{item?.rank}</td> : null}
+              <td>
+                <div
+                  className="d-flex position-relative rounded-circle gap-1"
+                  style={{ width: "25px", aspectRatio: "1/1" }}
+                >
+                  <img
+                    alt={item?.description?.moniker}
+                    className="rounded-circle"
+                    layout="fill"
+                    src={`/validatorAvatars/${item?.operatorAddress}.png`}
+                    onError={handleOnError}
+                  />
+                </div>
+              </td>
+              <td className=" d-flex align-items-center justify-content-start">
+                <a
+                  className="text-truncate"
+                  href={`https://explorer.assetmantle.one/validators/${item.operatorAddress}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   {" "}
-                  {getBalanceStyle(
-                    fromChainDenom(
-                      delegatedValidators?.find(
-                        (element) =>
-                          element?.operatorAddress == item?.operatorAddress
-                      )?.delegatedAmount
-                    ),
-                    "caption2 text-white-300",
-                    "small text-white-300"
-                  ) || "-"}
-                </td>
-              </tr>
-            ))
+                  {item?.description?.moniker}{" "}
+                  <i className="bi bi-arrow-up-right" />
+                </a>
+              </td>
+              <td>{((item?.tokens * 100) / totalTokens).toFixed(2)}%</td>
+
+              <td>
+                {shiftDecimalPlaces(item?.commission?.commissionRates?.rate, 2)}{" "}
+                %
+              </td>
+              <td>
+                {" "}
+                {getBalanceStyle(
+                  fromChainDenom(item?.am_delegatedAmount),
+                  "caption2 text-white-300",
+                  "small text-white-300"
+                ) || "-"}
+              </td>
+            </tr>
+          ))
         : delegatedValidators
             ?.filter(
               (item) =>
@@ -133,7 +179,6 @@ const DelegatedValidators = ({
                   ?.toLowerCase()
                   ?.includes(searchValue.toLowerCase())
             )
-
             ?.map((item, index) => (
               <tr key={index} className="caption2 text-white-300">
                 <td>
